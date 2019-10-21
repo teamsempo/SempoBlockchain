43b7878f765dc5a8b4787f10413d88a8ad47ebd0
Nick Williams <enjeyw@gmail.com>
2019-10-21 22:02:04 +1100

    oops forgot migrations

---
+++ app/migrations/versions/fea16694457a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: fea16694457a
+Revises: 35aa30ce8a33, 2e625c6545c1
+Create Date: 2019-10-19 15:48:18.598898
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fea16694457a'
+down_revision = ('35aa30ce8a33', '2e625c6545c1')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
