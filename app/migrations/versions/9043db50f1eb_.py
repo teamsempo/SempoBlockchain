b77d6a8700b70d2689873323dd714649ea48af25
Nick Williams <enjeyw@gmail.com>
2019-11-10 11:04:21 +1100

    head merge migrations

---
+++ app/migrations/versions/9043db50f1eb_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9043db50f1eb
+Revises: d1e9ab6c872c, ac8716ecdc21
+Create Date: 2019-11-10 11:03:51.414927
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9043db50f1eb'
+down_revision = ('d1e9ab6c872c', 'ac8716ecdc21')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
