a05f13684b8224da72e637e0526874a84f31d15c
Nick Williams <enjeyw@gmail.com>
2019-11-10 17:46:44 +1100

    testing sops hooks

--- .githooks/post_checkout.py
+++ .githooks/post_checkout.py
@@ -5,3 +5,5 @@ import os
 for path in glob.glob('./.sempoconfig/*.ini'):
     head, filename = os.path.split(path)
     subprocess.Popen(['sops', '-d', path, '>', f'./test_config_files/{filename}'])
+
+# Making a random change
