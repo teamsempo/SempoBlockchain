de99f5d65845cf1263ff15a41388c8943f098689
Nick Williams <enjeyw@gmail.com>
2019-11-10 19:40:11 +1100

    testing sops hooks

--- .githooks/post_checkout.py
+++ .githooks/post_checkout.py
@@ -2,8 +2,9 @@ import glob
 import subprocess
 import os
 
-for path in glob.glob('./.sempoconfig/*.ini'):
-    head, filename = os.path.split(path)
-    subprocess.Popen(['sops', '-d', path, '>', f'./test_config_files/{filename}'])
+for in_path in glob.glob('./*.ini'):
+    head, filename = os.path.split(in_path)
+    out_path = '../test_config_files/{}'.format(filename)
 
-# Making a random changes
+    with open(out_path, 'w') as f:
+        subprocess.call(['sops', '-d', in_path], stdout=f)
--- .sempoconfig/SOPS_save.py
+++ .sempoconfig/SOPS_save.py
@@ -2,9 +2,9 @@ import glob
 import subprocess
 import os
 
-for in_path in glob.glob('./test_config_files/*.ini'):
+for in_path in glob.glob('../test_config_files/*.ini'):
     head, filename = os.path.split(in_path)
-    out_path = './.sempoconfig/{}'.format(filename)
+    out_path = './{}'.format(filename)
 
     with open(out_path, 'w') as f:
         subprocess.call(['sops', '-e', in_path], stdout=f)
