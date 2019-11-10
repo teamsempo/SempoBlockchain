8b04b58a9c51c28d164ad755b2aa8af6602a0185
Nick Williams <enjeyw@gmail.com>
2019-11-10 11:38:09 +1100

    fixing migrations properly this time

---
+++ app/migrations/versions/38e2894e2a8f_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 38e2894e2a8f
+Revises: e44e075decdf
+Create Date: 2019-11-10 11:19:12.556541
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '38e2894e2a8f'
+down_revision = 'e44e075decdf'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
