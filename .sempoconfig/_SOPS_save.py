582355132f786bc259df8ca1c9ddb6271723e699
Nick Williams <enjeyw@gmail.com>
2019-11-11 15:03:16 +1100

    skipping already encrypted files

--- .sempoconfig/_SOPS_save.py
+++ .sempoconfig/_SOPS_save.py
@@ -1,6 +1,16 @@
 import glob
 import subprocess
 import os
+import hashlib
+import configparser
+
+
+def shahash(fpath):
+    hash_sha = hashlib.sha1()
+    with open(fpath, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha.update(chunk)
+    return hash_sha.hexdigest()
 
 try:
     os.chdir('./.sempoconfig')
@@ -15,7 +25,31 @@ for in_path in glob.glob('../config_files/*.ini'):
     print('Found file:')
     print(filename)
 
+    new_hash = shahash(in_path)
+
     out_path = './{}'.format(filename)
 
-    with open(out_path, 'w') as f:
-        subprocess.call(['sops', '-e', in_path], stdout=f)
+    skip = False
+    try:
+        pre_paser = configparser.ConfigParser()
+        pre_paser.read(out_path)
+        orginal_hash = pre_paser['META']['data_hash']
+
+        if orginal_hash == new_hash:
+            print('File contents are identical, skipping')
+            skip = True
+    except KeyError:
+        pass
+
+    if not skip:
+
+        with open(out_path, 'w') as f:
+            subprocess.call(['sops', '-e', in_path], stdout=f)
+
+        post_paser = configparser.ConfigParser()
+        post_paser.read(out_path)
+        post_paser['META'] = {}
+        post_paser['META']['data_hash'] = new_hash
+
+        with open(out_path, 'w') as f:
+            post_paser.write(f)
