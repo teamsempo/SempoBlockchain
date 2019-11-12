6c530548b5ef25a1c6a1effd7087b02da9f161c8
Nick Williams <enjeyw@gmail.com>
2019-11-12 12:06:01 +1100

    docker tests now properly raise a failure at build stage if db migrations are not correct

--- app/_docker_app_script.sh
+++ app/_docker_app_script.sh
@@ -3,8 +3,19 @@
 cd src
 echo upgrading database
 python manage.py db upgrade
+
+ret=$?
+if [ "$ret" -ne 0 ]; then
+  exit $ret
+fi
+
 echo upgrading dataset
 python manage.py update_data
+
+ret=$?
+if [ "$ret" -ne 0 ]; then
+  exit $ret
+fi
 
 if [ "$CONTAINER_MODE" = 'TEST' ]; then
   python invoke_tests.py
