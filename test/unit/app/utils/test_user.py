75cea3597daa519bc55cd2774f7ffd450182a259
Philip Wafula <wanga.philip.devwork@gmail.com>
2019-11-17 19:52:58 +0300

    Kenya Ussd State Machine Test Fixes.  - Added tests for change_initial_pin and change_current_pin methods.  - Removed redundant pin authorization tests.  - Cleaned up initial_pin_authorization_state and new_pin_authorization_states (Removed user input attribute, since session data now has initial_pin)

--- test/unit/app/utils/test_user.py
+++ test/unit/app/utils/test_user.py
@@ -2,6 +2,12 @@
 This file (test_user.py) contains the unit tests for the user.py file in utils dir.
 """
 import pytest
+from functools import partial
+from helpers.user import UserFactory
+from faker.providers import phone_number
+from faker import Faker
+from server.utils import user
+from server.utils.user import change_initial_pin, change_current_pin
 
 
 # REDACTED: USERS NOW HAVE MULTIPLE TRANSFER ACCOUNTS
@@ -53,3 +59,40 @@ def test_save_device_info(save_device_info, new_sempo_admin_user, serial_number,
     assert device is not None
     assert device.user_id is new_sempo_admin_user.id
 
+
+fake = Faker()
+fake.add_provider(phone_number)
+phone = partial(fake.msisdn)
+unactivated_user = partial(UserFactory, is_activated=False)
+standard_user = partial(UserFactory, pin='0000')
+
+
+@pytest.mark.parametrize("user_factory, user_input, expected_new_pin, expected_activation_status",
+                         [(unactivated_user, "0000", "0000", True)])
+def test_change_initial_pin(mocker, test_client, init_database, user_factory, user_input, expected_new_pin,
+                            expected_activation_status):
+    ua_user = user_factory()
+    ua_user.phone = phone()
+    assert ua_user.pin is None
+    assert ua_user.is_activated is False
+
+    user.send_sms = mocker.MagicMock()
+    change_initial_pin(ua_user, user_input)
+
+    assert ua_user.verify_pin(expected_new_pin) is True
+    assert ua_user.is_activated is expected_activation_status
+    user.send_sms.assert_called_with(ua_user, 'successful_pin_change_sms')
+
+
+@pytest.mark.parametrize("user_factory, user_input, expected_new_pin",
+                         [(standard_user, "1111", "1111")])
+def test_change_current_pin(mocker, user_factory, user_input, expected_new_pin):
+    strd_user = user_factory()
+    strd_user.phone = phone()
+    assert strd_user.verify_pin('0000') is True
+
+    user.send_sms = mocker.MagicMock()
+    change_current_pin(strd_user, user_input)
+
+    assert strd_user.verify_pin(expected_new_pin) is True
+    user.send_sms.assert_called_with(strd_user, 'successful_pin_change_sms')
