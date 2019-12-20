64427405fbd0d1b94d5101aa459061bb78191015
Nick Williams <enjeyw@gmail.com>
2019-12-20 16:43:04 +1100

    sneaky fix to get ride of all the ghost transfer accounts polluting the ui lists

--- app/server/utils/transfer_enums.py
+++ app/server/utils/transfer_enums.py
@@ -17,7 +17,7 @@ class TransferSubTypeEnum(enum.Enum):
     AGENT_OUT       = 'AGENT_OUT'
     FEE             = 'FEE'
     INCENTIVE       = 'INCENTIVE'
-    
+
 
 class TransferModeEnum(enum.Enum):
     NFC = "NFC"
