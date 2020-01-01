8861f9f638e8e0664a6c7d2724de6ec77c7991bb
tristanhcole <tristanhcole@gmail.com>
2020-01-01 22:22:22 +1100

    refactoring out test model into individual files

---
+++ test/unit/app/models/test_credit_transfer.py
@@ -0,0 +1,158 @@
+import pytest
+from server.exceptions import AccountLimitError
+from server.utils.transfer_enums import TransferSubTypeEnum, TransferTypeEnum
+
+
+def test_new_credit_transfer_complete(create_credit_transfer):
+    """
+    GIVEN a CreditTransfer model
+    WHEN a new credit transfer is created
+    THEN check transfer status is PENDING, then resolve as complete
+    """
+    from server.utils.transfer_enums import TransferStatusEnum
+    from flask import g
+    g.celery_tasks = []
+    assert isinstance(create_credit_transfer.transfer_amount, float)
+    assert create_credit_transfer.transfer_amount == 1000
+    assert create_credit_transfer.transfer_status is TransferStatusEnum.PENDING
+    create_credit_transfer.resolve_as_completed()  # complete credit transfer
+    assert create_credit_transfer.transfer_status is TransferStatusEnum.COMPLETE
+
+
+def test_new_credit_transfer_rejected(create_credit_transfer):
+    """
+    GIVEN a CreditTransfer model
+    WHEN a new credit transfer is created
+    THEN check transfer status is PENDING, then resolve as rejected with message,
+         check status is REJECTED and message is not NONE
+    """
+    from server.utils.transfer_enums import TransferStatusEnum
+    assert create_credit_transfer.transfer_status is TransferStatusEnum.PENDING
+
+    create_credit_transfer.resolve_as_rejected(
+        message="Sender {} has insufficient balance".format(create_credit_transfer.sender_transfer_account)
+    )  # reject credit transfer
+
+    assert create_credit_transfer.transfer_status is TransferStatusEnum.REJECTED
+    assert create_credit_transfer.resolution_message is not None
+
+
+def test_new_credit_transfer_check_sender_transfer_limits(create_credit_transfer):
+    """
+    GIVEN a CreditTransfer model
+    WHEN a new credit transfer is created
+    THEN check the correct check_sender_transfer_limits apply
+    """
+    from server.utils.transfer_limits import LIMITS
+    from server.models.kyc_application import KycApplication
+    from server.models import token
+
+    # Sempo Level 0 LIMITS (payment only)
+    assert create_credit_transfer.check_sender_transfer_limits() == [
+        limit for limit in LIMITS
+        if 'Sempo Level 0: P' in limit.name
+    ]
+
+    # Check Sempo Level 1 LIMITS (payment only)
+    create_credit_transfer.sender_user.is_phone_verified = True
+    assert create_credit_transfer.check_sender_transfer_limits() == [
+        limit for limit in LIMITS
+        if 'Sempo Level 1: P' in limit.name
+    ]
+
+    # Check Sempo Level 2 LIMITS (payment only)
+    kyc = KycApplication(type='INDIVIDUAL')
+    kyc.user = create_credit_transfer.sender_user
+    kyc.kyc_status = 'VERIFIED'
+    assert create_credit_transfer.check_sender_transfer_limits() == [
+        limit for limit in LIMITS
+        if 'Sempo Level 2: P' in limit.name
+    ]
+
+    # Check Sempo Level 3 LIMITS (payment only)
+    kyc.type = 'BUSINESS'
+    assert create_credit_transfer.check_sender_transfer_limits() == [
+        limit for limit in LIMITS
+        if 'Sempo Level 3: P' in limit.name
+    ]
+
+    # Check additional GE LIMITS for Liquid Token (withdrawal)
+    create_credit_transfer.token.token_type = token.TokenType.LIQUID
+    create_credit_transfer.transfer_type = TransferTypeEnum.WITHDRAWAL
+    create_credit_transfer.sender_transfer_account.balance = 10000
+    assert create_credit_transfer.check_sender_transfer_limits() == [
+        limit for limit in LIMITS
+        if 'GE Liquid Token - Standard User' == limit.name or 'Sempo Level 3: WD' in limit.name
+    ]
+
+    # Check additional GE LIMITS for Liquid Token (payment, Agent Out Subtype)
+    create_credit_transfer.token.token_type = token.TokenType.LIQUID
+    create_credit_transfer.transfer_type = TransferTypeEnum.PAYMENT
+    create_credit_transfer.transfer_subtype = TransferSubTypeEnum.AGENT_OUT
+    create_credit_transfer.sender_transfer_account.balance = 10000
+    assert create_credit_transfer.check_sender_transfer_limits() == [
+        limit for limit in LIMITS
+        if 'GE Liquid Token - Standard User' in limit.name or 'Sempo Level 3: WD' in limit.name
+    ]
+
+    # Check additional GE LIMITS for Liquid Token and Group Account (payment, Agent Out Subtype)
+    create_credit_transfer.sender_user.set_held_role('GROUP_ACCOUNT', 'grassroots_group_account')
+    create_credit_transfer.token.token_type = token.TokenType.LIQUID
+    create_credit_transfer.transfer_type = TransferTypeEnum.PAYMENT
+    create_credit_transfer.transfer_subtype = TransferSubTypeEnum.AGENT_OUT
+    create_credit_transfer.sender_transfer_account.balance = 10000
+    assert create_credit_transfer.check_sender_transfer_limits() == [
+        limit for limit in LIMITS
+        if 'GE Liquid Token - Group Account User' in limit.name or 'Sempo Level 3: WD' in limit.name
+    ]
+
+    # Check Limits skipped if no sender user (exchange)
+    create_credit_transfer.sender_user = None
+    create_credit_transfer.transfer_type = TransferTypeEnum.EXCHANGE
+    assert create_credit_transfer.check_sender_transfer_limits() is None
+
+
+def test_new_credit_transfer_check_sender_transfer_limits_exception(external_reserve_token, create_credit_transfer):
+    """
+    GIVEN a CreditTransfer model
+    WHEN a new credit transfer is created
+    THEN check the correct check_sender_transfer_limits raises AccountLimitError
+    """
+    from server.utils.transfer_limits import LIMITS
+    from server.models import credit_transfer, token
+    from server import db
+
+    create_credit_transfer.token.token_type = token.TokenType.RESERVE
+    create_credit_transfer.sender_user.kyc_applications = []
+
+    # Sempo Level 0 LIMITS (payment only) on init
+    with pytest.raises(AccountLimitError):
+        c = credit_transfer.CreditTransfer(
+            amount=1000000,
+            token=external_reserve_token,
+            sender_user=create_credit_transfer.sender_user,
+            recipient_user=create_credit_transfer.sender_user,
+            transfer_type=TransferTypeEnum.PAYMENT,
+            transfer_subtype=TransferSubTypeEnum.STANDARD
+        )
+        db.session.add(c)
+        c.resolve_as_completed()
+        db.session.flush()
+
+    # Sempo Level 0 LIMITS (payment only) on check LIMITS
+    with pytest.raises(AccountLimitError):
+        create_credit_transfer.check_sender_transfer_limits()
+
+    # Check GE LIMITS for Liquid Token (withdrawal) on check LIMITS
+    create_credit_transfer.token.token_type = token.TokenType.LIQUID
+    create_credit_transfer.transfer_type = TransferTypeEnum.WITHDRAWAL
+    create_credit_transfer.sender_transfer_account.balance = 1000
+    with pytest.raises(AccountLimitError):
+        create_credit_transfer.check_sender_transfer_limits()
+
+    # Check GE LIMITS for Liquid Token (payment, agent_out subtype) on check LIMITS
+    create_credit_transfer.transfer_type = TransferTypeEnum.PAYMENT
+    create_credit_transfer.transfer_subtype = TransferSubTypeEnum.AGENT_OUT
+    create_credit_transfer.sender_transfer_account.balance = 1000
+    with pytest.raises(AccountLimitError):
+        create_credit_transfer.check_sender_transfer_limits()
---
+++ test/unit/app/models/test_transfer_account.py
@@ -0,0 +1,94 @@
+import pytest
+
+
+def test_create_transfer_account(create_transfer_account):
+    """
+    GIVEN A transfer account model
+    WHEN a new transfer account is created
+    THEN check a blockchain address is created, default balance is 0
+    """
+    assert create_transfer_account.balance == 0
+    assert create_transfer_account.blockchain_address is not None
+
+
+def test_approve_beneficiary_transfer_account(create_transfer_account_user):
+    """
+    GIVEN a Transfer Account model
+    WHEN a new transfer account is created AND approved
+    THEN check a BENEFICIARY is disbursed initial balance
+    """
+    import config
+    create_transfer_account_user.transfer_account.is_beneficiary = True
+    create_transfer_account_user.transfer_account.approve_and_disburse()
+
+    assert create_transfer_account_user.transfer_account.balance == config.STARTING_BALANCE
+
+
+def test_approve_vendor_transfer_account(new_transfer_account):
+    """
+    GIVEN a Transfer Account model
+    WHEN a new transfer account is created and approved
+    THEN check a VENDOR is NOT disbursed initial balance
+    """
+    new_transfer_account.is_vendor = True
+    new_transfer_account.approve_and_disburse()
+
+    assert new_transfer_account.balance == 0
+
+
+@pytest.mark.parametrize("initial_bal, increment_amount, expected_final_bal", [
+    (0, 0.0, 0.0),
+    (5, 0.5, 5.5),
+    (0.1, 0.00001, 0.10001),
+    (0, -0.0, 0.0),
+    (5, -0.5, 4.5),
+    (0.1, - 0.00001, 0.09999),
+])
+def test_increment_balance(test_client, init_database, create_master_organisation,
+                           initial_bal, increment_amount, expected_final_bal):
+    """
+    Tests to ensure that adding and subtracting floats doesn't destroy decimal amounts, esp after db commit
+    """
+    import server.models.transfer_account
+    from server import db
+    ta = server.models.transfer_account.TransferAccount()
+    ta.balance = initial_bal
+    db.session.add(ta)
+    db.session.commit()
+    id = ta.id
+    db.session.expire(ta)
+
+    ta_queried = server.models.transfer_account.TransferAccount.query.execution_options(show_all=True).get(id)
+
+    ta_queried.increment_balance(increment_amount)
+
+    assert ta_queried.balance == expected_final_bal
+
+
+@pytest.mark.parametrize("initial_bal, decrement_amount, expected_final_bal", [
+    (0, 0.0, 0.0),
+    (5, 0.5, 4.5),
+    (0.1, 0.00001, 0.09999),
+    (0, -0.0, 0.0),
+    (5, -0.5, 5.5),
+    (0.1, - 0.00001, 0.10001),
+])
+def test_decrement_balance(test_client, init_database, create_master_organisation,
+                           initial_bal, decrement_amount, expected_final_bal):
+    """
+    Tests to ensure that adding and subtracting floats doesn't destroy decimal amounts, esp after db commit
+    """
+    import server.models.transfer_account
+    from server import db
+    ta = server.models.transfer_account.TransferAccount()
+    ta.balance = initial_bal
+    db.session.add(ta)
+    db.session.commit()
+    id = ta.id
+    db.session.expire(ta)
+
+    ta_queried = server.models.transfer_account.TransferAccount.query.execution_options(show_all=True).get(id)
+
+    ta_queried.decrement_balance(decrement_amount)
+
+    assert ta_queried.balance == expected_final_bal
