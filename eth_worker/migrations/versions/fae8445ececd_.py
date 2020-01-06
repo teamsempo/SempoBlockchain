f52126f8e8ac607311f20249fda6185c4fddf949
Nick Williams <enjeyw@gmail.com>
2020-01-06 20:16:52 +1100

    db head merge

---
+++ eth_worker/migrations/versions/fae8445ececd_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: fae8445ececd
+Revises: 9c688d9f23a6
+Create Date: 2020-01-06 20:16:10.276638
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fae8445ececd'
+down_revision = '9c688d9f23a6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
