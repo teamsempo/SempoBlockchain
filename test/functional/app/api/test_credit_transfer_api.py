81bc2d21e9d14f82250603adae3ea2b1cf346144
tristanhcole <tristanhcole@gmail.com>
2020-01-14 20:10:17 +1100

    credit transfer tests (get)

--- test/functional/app/api/test_credit_transfer_api.py
+++ test/functional/app/api/test_credit_transfer_api.py
@@ -8,7 +8,7 @@ import pytest
     (lambda o: 1222103, 404),
     (lambda o: None, 200)
 ])
-def test_get_single_credit_transfer(test_client, complete_admin_auth_token, create_credit_transfer,
+def test_get_credit_transfer(test_client, complete_admin_auth_token, create_credit_transfer,
                           credit_transfer_selector_func, status_code):
     response = test_client.get(
         f"/api/v1/credit_transfer/{credit_transfer_selector_func(create_credit_transfer) or ''}",
