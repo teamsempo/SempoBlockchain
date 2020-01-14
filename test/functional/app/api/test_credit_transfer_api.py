8036a848321c4c206cdcb3960830fb8997e1dec6
tristanhcole <tristanhcole@gmail.com>
2020-01-14 20:10:09 +1100

    credit transfer tests (get)

--- test/functional/app/api/test_credit_transfer_api.py
+++ test/functional/app/api/test_credit_transfer_api.py
@@ -1,20 +1,24 @@
 import pytest
 
-# todo- get credit transfers / post credit transfers / put credit transfer
+# todo- post credit transfers / put credit transfer
 
 
 @pytest.mark.parametrize("credit_transfer_selector_func, status_code", [
     (lambda o: o.id, 200),
     (lambda o: 1222103, 404),
+    (lambda o: None, 200)
 ])
 def test_get_single_credit_transfer(test_client, complete_admin_auth_token, create_credit_transfer,
                           credit_transfer_selector_func, status_code):
     response = test_client.get(
-        f"/api/v1/credit_transfer/{credit_transfer_selector_func(create_credit_transfer) or ""}",
+        f"/api/v1/credit_transfer/{credit_transfer_selector_func(create_credit_transfer) or ''}",
         headers=dict(
             Authorization=complete_admin_auth_token,
             Accept='application/json'
         ))
 
     assert response.status_code == status_code
+
+    if not credit_transfer_selector_func(create_credit_transfer):
+        assert isinstance(response.json['data']['credit_transfers'], list)
 
