75283c13e7f89b3bad8e0704a62143c6a2e79355
michielderoos <michiel@Michiels-MacBook-Pro.local>
2020-01-20 16:55:02 -0400

    Make enable configurable

--- app/server/utils/task_runner.py
+++ app/server/utils/task_runner.py
@@ -1,19 +1,10 @@
 from . import worker_simulator
+from flask import current_app
 from server import celery_app
 
 def delay_task(task, kwargs=None, args=None):
-    print('sim starting')
-    print(task)
-    print(args)
-    sim_resp = worker_simulator.simulate(task, kwargs, args)
-    print('SIM RESPONSE')
-    print(sim_resp)
+    if current_app.config['ENABLE_SIMULATOR_MODE']:
+        return worker_simulator.simulate(task, kwargs, args)
     signature = celery_app.signature(task, kwargs=kwargs, args=args)
-    print(signature)
-    print(task)
-    print(args)
     async_result = signature.delay()
-    print('REAL RESPONSE')
-    print(async_result)
-    print(type(async_result))
     return async_result
