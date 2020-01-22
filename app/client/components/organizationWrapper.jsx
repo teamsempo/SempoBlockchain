44ce6b84a96c59c98d6feabb1b1c8668417c4517
Eric <eeoneric@gmail.com>
2020-01-22 10:55:10 -0500

    shortened query params

--- app/client/components/organizationWrapper.jsx
+++ app/client/components/organizationWrapper.jsx
@@ -11,10 +11,20 @@ const OrganizationWrapper = Component => class extends Component {
         //load organisation if param is not matching currently active org
         let query_params = parseQuery(location.search)
         if(query_params["org"] && this.props.login.organisationId != query_params["org"]){
-            this.props.updateActiveOrgRequest(query_params["org_name"], query_params["org"])
+            let org_id = query_params["org"]
+            // retrieve org of "org_id" from state
+            let org = this.props.login.organisations.find(e => e.id == org_id)
+
+            // if valid org, update active org otherwise redirect to root path
+            if(org) {
+                this.props.updateActiveOrgRequest(org.name, org_id)
+            } else {
+                window.location.assign('/')
+            }
+
         } else {
             this.props.history.replace({
-                search: generateQueryString({org_name: this.props.login.organisationName})
+                search: generateQueryString()
             })
         }
         
