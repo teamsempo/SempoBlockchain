1f0e2466ffc16b088489e3e93afe478d918f4306
Michiel <michiel@deroos.ca>
2020-02-02 13:05:28 -0500

    Add pepper to hashes (#125)
    * Sketch some design notes, pseudocode some stuff
    
    * Make some notes
    
    * check git setting
    
    * Revert "Sketch some design notes, pseudocode some stuff"
    
    This reverts commit a59f2f7f01796f11872038fb2810af8c46b801b1.
    
    * Works for new accounts
    
    * db migration
    
    * Undo silly change
    
    * pepper pepper pepper
    
    * DB Migration, Read from settings
    
    * drop current_app dep from config
    
    * Update tests
    
    * Config
    
    * Tidy migrations!
    
    * Fix lil' migrations issue caused by merging in master
    
    * derp
    
    * bumping version
    
    Co-authored-by: Nick Williams <enjeyw@gmail.com>

---
+++ app/migrations/versions/961ab9adc300_.py
@@ -0,0 +1,37 @@
+"""Adds external auth password
+
+Revision ID: 961ab9adc300
+Revises: df0fbf496520
+Create Date: 2020-01-25 17:29:33.195793
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.orm import Session
+from server.models.organisation import Organisation
+from server.utils.misc import encrypt_string
+import secrets
+
+# revision identifiers, used by Alembic.
+revision = '961ab9adc300'
+down_revision = 'df0fbf496520'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+    session = Session(bind=conn)
+
+    op.add_column('organisation', sa.Column('external_auth_username', sa.String(), nullable=True))
+    op.add_column('organisation', sa.Column('external_auth_password', sa.String(), nullable=True))
+
+    for org in session.query(Organisation).execution_options(show_all=True).all():
+        org.external_auth_username = 'admin_'+org.name.lower().replace(' ', '_')
+        org.external_auth_password = encrypt_string(secrets.token_hex(16))
+    session.commit()
+
+
+def downgrade():
+    op.drop_column('organisation', 'external_auth_username')
+    op.drop_column('organisation', 'external_auth_password')
