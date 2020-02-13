53f3120e6fbf48b25800de9558f8cbc45f5f1f3a
michielderoos <michiel@deroos.ca>
2020-02-13 13:16:52 -0400

    Trigger

--- app/migrations/versions/1c4c356aa44c_.py
+++ app/migrations/versions/1c4c356aa44c_.py
@@ -1,4 +1,4 @@
-"""empty message
+"""Adds search_view with indexes
 
 Revision ID: 1c4c356aa44c
 Revises: fc2eeba8e8e8
@@ -44,13 +44,33 @@ def upgrade():
     '''))
 
     op.create_index(op.f('ix_search_view_id'), 'search_view', ['id'], unique=True)
-
     op.create_index(op.f('ix_tsv_email'), 'search_view', ['tsv_email'], postgresql_using='gin')
     op.create_index(op.f('ix_tsv_phone'), 'search_view', ['tsv_phone'], postgresql_using='gin')
     op.create_index(op.f('ix_tsv_firstname'), 'search_view', ['tsv_first_name'], postgresql_using='gin')
     op.create_index(op.f('ix_tsv_lastname'), 'search_view', ['tsv_last_name'], postgresql_using='gin')
 
+    # Function to refresh index
+    conn.execute(sa.sql.text('''
+        CREATE OR REPLACE FUNCTION trig_refresh_search_view() RETURNS trigger AS
+        $$
+        BEGIN
+            REFRESH MATERIALIZED VIEW CONCURRENTLY search_view;
+            RETURN NULL;
+        END;
+        $$
+        LANGUAGE plpgsql ;
+    '''))
+
+    # Make the index update itself when users are changed (using above function)
+    conn.execute(sa.sql.text('''DROP TRIGGER IF EXISTS search_trigger ON "user"'''))
+    conn.execute(sa.sql.text('''
+            CREATE TRIGGER search_trigger AFTER TRUNCATE OR INSERT OR DELETE OR UPDATE
+            ON "user" FOR EACH STATEMENT
+            EXECUTE PROCEDURE trig_refresh_search_view()
+        '''))
+            
 def downgrade():
     conn = op.get_bind()
-    # drop the materialized view
+    # drop the materialized view and old trigger
     conn.execute(sa.sql.text('DROP MATERIALIZED VIEW search_view'))
+    conn.execute(sa.sql.text('''DROP TRIGGER IF EXISTS search_trigger ON "user"'''))
