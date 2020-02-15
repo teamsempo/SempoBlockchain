c9cb50071a4a733500bd9e5a2ca639e1f198ebe3
michielderoos <michiel@deroos.ca>
2020-02-14 23:28:21 -0400

    a

--- test/conftest.py
+++ test/conftest.py
@@ -442,7 +442,8 @@ def init_database():
 
     with current_app.app_context():
         try:
-            db.session.execute('DROP MATERIALIZED VIEW IF EXISTS search_view CASCADE;')
+            db.session.execute('DROP MATERIALIZED VIEW IF EXISTS search_view;')
+            db.session.commit()
         except:
             pass
         db.session.remove()  # DO NOT DELETE THIS LINE. We need to close sessions before dropping tables.
