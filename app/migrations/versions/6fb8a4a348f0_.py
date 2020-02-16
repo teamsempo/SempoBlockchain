69f44990c20bbb05e8556a2add056057d34d79f9
Philip Wafula <wanga.philip.devwork@gmail.com>
2020-02-16 16:07:14 +0300

    Resolve migration issues [Uncertain about this approach]

--- app/migrations/versions/6fb8a4a348f0_.py
+++ app/migrations/versions/6fb8a4a348f0_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '6fb8a4a348f0'
-down_revision = 'fc2eeba8e8e8'
+down_revision = '326a61cf9fde'
 branch_labels = None
 depends_on = None
 
