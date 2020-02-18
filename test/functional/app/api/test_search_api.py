0b8f7623ab30b8d8ec357d40bd65da1e8e88dcea
michielderoos <michiel@deroos.ca>
2020-02-18 11:02:55 -0400

    Update test

--- test/functional/app/api/test_search_api.py
+++ test/functional/app/api/test_search_api.py
@@ -24,11 +24,18 @@ def test_search_api(test_client, complete_admin_auth_token, create_organisation)
                 u._phone,
                 u.first_name,
                 u.last_name,
+                u._public_serial_number,
+                u._location,
+                u.primary_blockchain_address,
                 u.default_transfer_account_id,
                 to_tsvector(u.email) AS tsv_email,
                 to_tsvector(u._phone) AS tsv_phone,
                 to_tsvector(u.first_name) AS tsv_first_name,
-                to_tsvector(u.last_name) AS tsv_last_name
+                to_tsvector(u.last_name) AS tsv_last_name,
+                to_tsvector(u._public_serial_number) AS tsv_public_serial_number,
+                to_tsvector(u._location) AS tsv_location,
+                to_tsvector(u.primary_blockchain_address) AS tsv_primary_blockchain_address,
+                to_tsvector(CAST (u.default_transfer_account_id AS VARCHAR(10))) AS tsv_default_transfer_account_id
             FROM "user" u
         );
     ''')
