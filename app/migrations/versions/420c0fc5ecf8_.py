142c15ff229694622589fdcbde1070fb41bb12ed
philip <philipwafula2@gmail.com>
2020-03-03 14:39:56 +0300

    Merged heads to resolve migration conflicts.

---
+++ app/migrations/versions/420c0fc5ecf8_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 420c0fc5ecf8
+Revises: c709ae4679ad, 7c954be04a0c
+Create Date: 2020-03-03 12:53:33.473560
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '420c0fc5ecf8'
+down_revision = ('c709ae4679ad', '7c954be04a0c')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
