7d651b71b3b9941ad503a946cf508d798f15bb08
michielderoos <michiel@deroos.ca>
2020-03-04 20:20:37 -0400

    lint

--- pgbouncer/entrypoint.sh
+++ pgbouncer/entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 # Based on https://raw.githubusercontent.com/brainsam/pgbouncer/master/entrypoint.sh
-
 set -e
-
-# Here are some parameters. See all on
-# https://pgbouncer.github.io/config.html
-
 PG_CONFIG_DIR=/etc/pgbouncer
 echo "Creating sempo pgbouncer config"
 python3 generate_pgbouncer_config.py
-
 exec "$@"
