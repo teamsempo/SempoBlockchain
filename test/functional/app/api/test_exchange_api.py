607dabb58f531b8738d9f1a3ff811af7c51db6a9
michielderoos <michiel@deroos.ca>
2020-03-13 13:31:53 -0300

    .

--- test/functional/app/api/test_exchange_api.py
+++ test/functional/app/api/test_exchange_api.py
@@ -35,7 +35,4 @@ def test_exchange(test_client, user_with_reserve_balance, initialised_blockchain
     if status_code == 200 and will_func_test_blockchain():
         task_uuid = response.json['data']['exchange']['blockchain_task_uuid']
         result = bt.await_task_success(task_uuid, timeout=config.SYNCRONOUS_TASK_TIMEOUT * 24)
-        print('-----')
-        print(result)
-        print(result['status'])
         assert result['status'] == 'SUCCESS'
