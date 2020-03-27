4ad1231cd8525c920f957ada64d4b4b04ee8cafa
Nick Williams <enjeyw@gmail.com>
2020-03-27 18:02:23 +1100

    oops wrong profile

--- prepare_aws_deploy.sh
+++ prepare_aws_deploy.sh
@@ -4,7 +4,7 @@ bash build_docker.sh
 
 sed "s|REPOSITORY_URI|$REPOSITORY_URI|g" awsDockerrunTemplate.json > Dockerrun.aws.json
 
-eval $(aws ecr get-login --no-include-email --region $ECR_REGION --profile SARAFU);
+eval $(aws ecr get-login --no-include-email --region $ECR_REGION --profile ECR_BUILDER);
 
 docker tag server:latest $REPOSITORY_URI:server
 docker push $REPOSITORY_URI:server
