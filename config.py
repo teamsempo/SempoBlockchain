976a46574916daccaba43ee31271aff079210c67
Nick Williams <enjeyw@gmail.com>
2020-04-01 10:53:05 +1100

    tweaks to deployment: - secrets bucket can be specified via env file - local build process also retrieves git-hash tagged docker images

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ if load_from_s3:
 
     client = session.client('s3')
 
-    SECRET_BUCKET = "ctp-prod-secrets"
+    SECRET_BUCKET = os.environ.get("SECRETS_BUCKET", "ctp-prod-secrets")
     FORCE_SSL = True
 
     common_obj = client.get_object(Bucket=SECRET_BUCKET, Key=COMMON_FILENAME)
