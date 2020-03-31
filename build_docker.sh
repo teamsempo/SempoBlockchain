d565103c079b8924d09bb60c83dfb084d6573631
michielderoos <michiel@deroos.ca>
2020-03-31 16:00:38 -0300

    try

--- build_docker.sh
+++ build_docker.sh
@@ -10,6 +10,7 @@ cd ../
 
 docker build -t server . -f ./app/Dockerfile --build-arg GIT_HASH=$GIT_HASH
 docker build -t proxy ./proxy
+docker build -t eth_worker . -f ./eth_worker/Dockerfile	--build-arg CONTAINER_TYPE=ANY_PRIORITY_WORKER
 docker build -t low_priority_eth_worker . -f ./eth_worker/Dockerfile --build-arg CONTAINER_TYPE=LOW_PRIORITY_WORKER
 docker build -t high_priority_eth_worker . -f ./eth_worker/Dockerfile --build-arg CONTAINER_TYPE=HIGH_PRIORITY_WORKER
 docker build -t pgbouncer . -f ./pgbouncer/Dockerfile
