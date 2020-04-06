50cbacb06dab9097a7c575a1fb81c3dae13b9416
tristanhcole <tristanhcole@gmail.com>
2020-04-06 13:39:34 +1000

    - utils.test.js - adding local storage mock

--- app/client/__tests__/setup/setupTests.js
+++ app/client/__tests__/setup/setupTests.js
@@ -1,4 +1,29 @@
+import "isomorphic-fetch";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
