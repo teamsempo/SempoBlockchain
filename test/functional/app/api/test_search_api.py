963d37474fac1e2b70cc58dfab8ddd6615849141
michielderoos <michiel@deroos.ca>
2020-04-07 19:41:32 -0300

    Fix search tests

--- test/functional/app/api/test_search_api.py
+++ test/functional/app/api/test_search_api.py
@@ -7,12 +7,7 @@ from server.utils.auth import get_complete_auth_token
 from server.utils.user import create_transfer_account_user, set_custom_attributes
 from server import db
 
-def test_search_api(test_client, complete_admin_auth_token, create_organisation):
-    """
-    When the '/api/v1/search/' page is requested with search parameters
-    check that the results are in the correct order
-    """
-
+def prep_search_api(test_client, complete_admin_auth_token, create_organisation):
     # This is a hack because the test DB isn't being built with migrations (and thus doesn't have tsvectors)
     db.session.execute("drop table search_view;")
     db.session.commit()
@@ -64,44 +59,53 @@ def test_search_api(test_client, complete_admin_auth_token, create_organisation)
     # Manually refresh tsvectors because the test DB has no triggers either
     db.session.execute("REFRESH MATERIALIZED VIEW search_view;")
 
-    # --Tests for standard search--
-    expected_results = {
-        '': ['Roy', 'Francine', 'Michiel'], # Empty string should return everyone
-        'fra': ['Francine'], # Only user starting with 'fra' substring is Francine
-        'fra der': ['Francine', "Michiel"], # 'fra der' should return Francine first. Michiel 2nd, because it still matches _something_
-        'mic der': ['Michiel', "Francine"] # 'fra der' should return Michiel first. Francine 2nd, because it still matches _something_
-    }
-    for search_term in expected_results:
-        response = test_client.get('/api/v1/search/?search_string={}&search_type=transfer_accounts'.format(search_term),
-                                headers=dict(
-                                Authorization=complete_admin_auth_token, Accept='application/json'),
-                                follow_redirects=True)
-        transfer_accounts = response.json['data']['transfer_accounts']
-        assert response.status_code == 200
-        user_names = []
-        for transfer_account in transfer_accounts:
-            if transfer_account['users']:
-                user_names.append(transfer_account['users'][0]['first_name'])
-        assert expected_results[search_term] == user_names
+@pytest.mark.parametrize("search_term, results", [
+    ('', ['Roy', 'Francine', 'Michiel']), # Empty string should return everyone
+    ('fra', ['Francine']), # Only user starting with 'fra' substring is Francine
+    ('fra der', ['Francine', "Michiel"]), # 'fra der' should return Francine first. Michiel 2nd, because it still matches _something_
+    ('mic der', ['Michiel', "Francine"]), # 'fra der' should return Michiel first. Francine 2nd, because it still matches _something_(
+])
+def normal_search(search_term, results, test_client, complete_admin_auth_token, create_organisation):
+    """
+    When the '/api/v1/search/' page is requested with search parameters
+    check that the results are in the correct order
+    """
 
-    # --Tests for filters and sorting--
-
-    expected_results = {
-        ('', '%$user_filters%,rounded_account_balance%<20%'): ['Roy', 'Francine', 'Michiel'],
-        ('', '%$user_filters%,rounded_account_balance%>100%'): [],
-    }
-    for (search_term, filters), expected_result in expected_results.items():
-        response = test_client.get('/api/v1/search/?search_string={}&search_type=transfer_accounts&params={}'.format(search_term, filters),
+    response = test_client.get('/api/v1/search/?search_string={}&search_type=transfer_accounts'.format(search_term),
                             headers=dict(
                             Authorization=complete_admin_auth_token, Accept='application/json'),
                             follow_redirects=True)
-        transfer_accounts = response.json['data']['transfer_accounts']
-        assert response.status_code == 200
-        user_names = []
-        for transfer_account in transfer_accounts:
-            if transfer_account['users']:
-                user_names.append(transfer_account['users'][0]['first_name'])
-        assert expected_result == user_names
+    transfer_accounts = response.json['data']['transfer_accounts']
+    assert response.status_code == 200
+    user_names = []
+    for transfer_account in transfer_accounts:
+        if transfer_account['users']:
+            user_names.append(transfer_account['users'][0]['first_name'])
+    assert results == user_names
+
+@pytest.mark.parametrize("search_term, filters, results", [
+    ('', '%$user_filters%,rounded_account_balance%<20%', ['Roy', 'Francine', 'Michiel']),
+    ('', '%$user_filters%,rounded_account_balance%>100%', []),
+])
+def filtered_search(search_term, filters, results, test_client, complete_admin_auth_token, create_organisation):
+    """
+    When the '/api/v1/search/' page is requested with filters
+    check that the results are in the correct order
+    """
+    response = test_client.get('/api/v1/search/?search_string={}&search_type=transfer_accounts&params={}'.format(search_term, filters),
+                        headers=dict(
+                        Authorization=complete_admin_auth_token, Accept='application/json'),
+                        follow_redirects=True)
+    transfer_accounts = response.json['data']['transfer_accounts']
+    assert response.status_code == 200
+    user_names = []
+    for transfer_account in transfer_accounts:
+        if transfer_account['users']:
+            user_names.append(transfer_account['users'][0]['first_name'])
+    assert results == user_names
+
+def tear_down():
     db.session.execute('DROP MATERIALIZED VIEW search_view CASCADE;')
+    db.session.flush()
     db.session.commit()
 
