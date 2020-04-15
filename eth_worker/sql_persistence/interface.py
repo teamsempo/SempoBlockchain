b759d8f5d9bbb4f7e5702151bd58d3bf37b0c4dc
michielderoos <michiel@deroos.ca>
2020-04-15 12:26:46 -0300

    refresh

--- eth_worker/sql_persistence/interface.py
+++ eth_worker/sql_persistence/interface.py
@@ -93,6 +93,7 @@ class SQLPersistenceInterface(object):
         print(f'Attempting lock for txn: {transaction_id} \n'
               f'addr:{signing_wallet_obj.address}')
         with lock:
+            self.session.refresh()
             return self.claim_transaction_nonce(signing_wallet_obj, transaction_id)
             
 
