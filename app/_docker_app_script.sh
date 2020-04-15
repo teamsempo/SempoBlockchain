ec2a227b9d5b5be53c78f4a3ae54355f2b162754
tristanhcole <tristanhcole@gmail.com>
2020-04-15 13:38:11 +1000

    test should fail2

--- app/_docker_app_script.sh
+++ app/_docker_app_script.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -eo pipefail
+#!/usr/bin/env bash
 
 cd src
 echo upgrading database
@@ -20,6 +20,10 @@ fi
 if [ "$CONTAINER_MODE" = 'TEST' ]; then
    #todo(COVERAGE): fix so that eth_worker is included
    coverage run invoke_tests.py
+   ret=$?
+   if [ "$ret" -ne 0 ]; then
+     exit $ret
+   fi
    bash <(curl -s https://codecov.io/bash) -cF python
 else
   uwsgi --socket 0.0.0.0:9000 --protocol http  --processes 4 --enable-threads --module=server.wsgi:app
