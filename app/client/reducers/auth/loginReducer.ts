teamsempo/SempoBlockchain

commit cabfa25007dfce27924f6c17dce18dc4022d5f09

Organisations defaults (#182)
* - adding _country_code, _default_disbursement_wei and require_transfer_card to organisation model
- adding ISO 3166-1 alpha-2 country codes

* - adding _country_code, _default_disbursement_wei and require_transfer_card to organisation model
- adding ISO 3166-1 alpha-2 country codes
- updating front-end to normalize organisations

* - removing default disbursement, require transfer card from config
- updating SOPS

* updatin tests

* fixing node build

* adding new configs to org post api

* building organisation PUT api

* adding lat/lng

* frontend

* edit organisation front end

* fixing typescript build

* removing from configs:

default_country
default_lat
default_lng
default_initial_disbursement
require_transfer_card_exists

* fixing migrations

* None type check

* None type check

* tests

* new feature package.json bump

app/client/reducers/auth/loginReducer.ts
@@ -20,16 +20,11 @@ interface LoginState {
   vendorId: null | number;
   intercomHash: null | string;
   webApiVersion: null | string;
-  organisationName: null | string;
-  //TODO(refactor): is it number or string?
   organisationId: null | number;
-  organisationToken: null | string;
   usdToSatoshiRate: null | number;
   error: null | string;
   tfaURL: null | string;
   tfaFailure: boolean;
-  //TODO(refactor): what is this actually?
-  organisations?: null | string[];
   requireTransferCardExists: null | boolean;
   adminTier?: string;
 }
@@ -42,10 +37,7 @@ const initialLoginState: LoginState = {
   vendorId: null,
   intercomHash: null,
   webApiVersion: null,
-  organisationName: null,
   organisationId: null,
-  organisationToken: null,
-  organisations: null,
   requireTransferCardExists: null,
   usdToSatoshiRate: null,
   error: null,
@@ -61,7 +53,6 @@ export const login = (state = initialLoginState, action: LoginAction) => {
     case UPDATE_ACTIVE_ORG:
       return {
         ...state,
-        organisationName: action.payload.organisationName,
         organisationId: action.payload.organisationId
       };
     case LOGIN_SUCCESS:
@@ -73,10 +64,7 @@ export const login = (state = initialLoginState, action: LoginAction) => {
         vendorId: action.vendorId,
         intercomHash: action.intercomHash,
         webApiVersion: action.webApiVersion,
-        organisationName: action.organisationName,
         organisationId: action.organisationId,
-        organisationToken: action.organisationToken,
-        organisations: action.organisations,
         requireTransferCardExists: action.requireTransferCardExists,
         email: action.email,
         adminTier: action.adminTier,
@@ -92,9 +80,7 @@ export const login = (state = initialLoginState, action: LoginAction) => {
         userId: null,
         intercomHash: null,
         webApiVersion: null,
-        organisationName: null,
         organisationId: null,
-        organisations: null,
         requireTransferCardExists: null,
         tfaURL: action.tfaURL,
         tfaFailure: action.tfaFailure,

app/client/reducers/auth/types.ts
@@ -4,9 +4,7 @@ export interface ReauthRequest {
 }
 export const UPDATE_ACTIVE_ORG = "UPDATE_ACTIVE_ORG";
 export interface UpdateActiveOrgPayload {
-  organisationName: string;
   organisationId: number;
-  organisationToken: string;
 }
 interface UpdateActiveOrg {
   type: typeof UPDATE_ACTIVE_ORG;
@@ -40,7 +38,7 @@ export interface LoginSuccess {
   intercomHash: null | string;
   webApiVersion: null | string;
   organisationName: null | string;
-  organisationId: null | number;
+  organisationId: number;
   organisationToken: null | string;
   usdToSatoshiRate: null | number;
   organisations?: string[];

app/client/sagas/authSagas.js
@@ -21,7 +21,11 @@ import {
   removeTFAToken,
   parseQuery
 } from "../utils";
-import { adminUserSchema, inviteUserSchema } from "../schemas";
+import {
+  adminUserSchema,
+  inviteUserSchema,
+  organisationSchema
+} from "../schemas";
 
 import {
   requestApiToken,
@@ -81,6 +85,7 @@ import {
 
 import { browserHistory } from "../app.jsx";
 import { ADD_FLASH_MESSAGE } from "../reducers/messageReducer";
+import { UPDATE_ORGANISATION_LIST } from "../reducers/organisation/types";
 
 function* updateStateFromAdmin(data) {
   //Schema expects a list of admin user objects
@@ -107,6 +112,23 @@ function* updateStateFromAdmin(data) {
 
   yield put({ type: UPDATE_ADMIN_USER_LIST, admins });
   yield put({ type: DEEP_UPDATE_INVITE_USER_LIST, invites });
+}
+
+export function* updateOrganisationStateFromLoginData(data) {
+  //Schema expects a list of organisation objects
+  let organisation_list;
+  if (data.organisations) {
+    organisation_list = data.organisations;
+  } else {
+    organisation_list = [data.organisation];
+  }
+
+  const normalizedData = normalize(organisation_list, organisationSchema);
+  const organisations = normalizedData.entities.organisations;
+
+  if (organisations) {
+    yield put({ type: UPDATE_ORGANISATION_LIST, organisations });
+  }
 }
 
 function* saveOrgId({ payload }) {
@@ -145,11 +167,7 @@ function createLoginSuccessObject(token) {
     usdToSatoshiRate: token.usd_to_satoshi_rate,
     intercomHash: token.web_intercom_hash,
     webApiVersion: token.web_api_version,
-    organisationName: token.active_organisation_name,
-    organisationId: token.active_organisation_id,
-    organisationToken: token.active_organisation_token,
-    organisations: token.organisations,
-    requireTransferCardExists: token.require_transfer_card_exists
+    organisationId: token.active_organisation_id
   };
 }
 
@@ -158,6 +176,7 @@ function* requestToken({ payload }) {
     const token_response = yield call(requestApiToken, payload);
 
     if (token_response.status === "success") {
+      yield call(updateOrganisationStateFromLoginData, token_response);
       yield put(createLoginSuccessObject(token_response));
       yield call(storeSessionToken, token_response.auth_token);
       yield call(authenticatePusher);
@@ -204,6 +223,7 @@ function* refreshToken() {
     yield put({ type: REAUTH_REQUEST });
     const token_request = yield call(refreshApiToken);
     if (token_request.auth_token) {
+      yield call(updateOrganisationStateFromLoginData, token_request);
       yield put(createLoginSuccessObject(token_request));
       yield call(storeSessionToken, token_request.auth_token);
       yield call(authenticatePusher);
@@ -242,6 +262,7 @@ function* register({ payload }) {
       });
       browserHistory.push("/login");
     } else if (registered_account.auth_token && !registered_account.tfa_url) {
+      yield call(updateOrganisationStateFromLoginData, registered_account);
       // email invite, auto login as email validated
       yield put({ type: REGISTER_SUCCESS, registered_account });
       yield put(createLoginSuccessObject(registered_account));
@@ -285,6 +306,7 @@ function* activate({ payload }) {
     const activated_account = yield call(activateAPI, payload);
 
     if (activated_account.auth_token && !activated_account.tfa_url) {
+      yield call(updateOrganisationStateFromLoginData, activated_account);
       yield put({ type: ACTIVATE_SUCCESS, activated_account });
       yield put(createLoginSuccessObject(activated_account));
       yield call(storeSessionToken, activated_account.auth_token);
@@ -443,6 +465,7 @@ function* validateTFA({ payload }) {
   try {
     const validateTFAresponse = yield call(ValidateTFAAPI, payload);
 
+    yield call(updateOrganisationStateFromLoginData, activated_account);
     yield put({ type: VALIDATE_TFA_SUCCESS, validateTFAresponse });
     yield call(storeTFAToken, validateTFAresponse.tfa_auth_token);
     yield put(createLoginSuccessObject(validateTFAresponse));

app/client/reducers/organisation/types.ts
@@ -1,32 +1,78 @@
 export interface Organisation {
+  id: number;
   name: string;
   token: {
     symbol: string;
   };
-}
-export const UPDATE_ORGANISATION = "UPDATE_ORGANISATION";
-interface UpdateOrganisation {
-  type: typeof UPDATE_ORGANISATION;
-  organisation: Organisation;
+  require_transfer_card: boolean;
+  default_disbursement: number;
+  country_code: string;
 }
 
-export type OrganisationAction = UpdateOrganisation;
+export const UPDATE_ORGANISATION_LIST = "UPDATE_ORGANISATION_LIST";
+
+interface UpdateOrganisationList {
+  type: typeof UPDATE_ORGANISATION_LIST;
+  organisations: Organisation;
+}
+
+export type OrganisationAction = UpdateOrganisationList;
+
+export const EDIT_ORGANISATION_REQUEST = "EDIT_ORGANISATION_REQUEST";
+
+export interface EditOrganisationPayload {
+  body: {
+    country_code: string;
+    default_disbursement: number;
+    require_transfer_card: boolean;
+    default_lat: number;
+    default_lng: number;
+  };
+  path: number;
+}
+
+interface EditOrganisationRequest {
+  type: typeof EDIT_ORGANISATION_REQUEST;
+  payload: EditOrganisationPayload;
+}
+
+export const EDIT_ORGANISATION_SUCCESS = "EDIT_ORGANISATION_SUCCESS";
+
+interface EditOrganisationSuccess {
+  type: typeof EDIT_ORGANISATION_SUCCESS;
+}
+
+export const EDIT_ORGANISATION_FAILURE = "EDIT_ORGANISATION_FAILURE";
+
+interface EditOrganisationFailure {
+  type: typeof EDIT_ORGANISATION_FAILURE;
+  error: string;
+}
+
+export type EditOrganisationAction =
+  | EditOrganisationRequest
+  | EditOrganisationSuccess
+  | EditOrganisationFailure;
+
 
 export const LOAD_ORGANISATION_REQUEST = "LOAD_ORGANISATIONS_REQUEST";
-interface LoadOrganisationRequest {
+
+interface LoadOrganisationsRequest {
   type: typeof LOAD_ORGANISATION_REQUEST;
 }
 export const LOAD_ORGANISATION_SUCCESS = "LOAD_ORGANISATIONS_SUCCESS";
-interface LoadOrganisationSuccess {
+
+interface LoadOrganisationsSuccess {
   type: typeof LOAD_ORGANISATION_SUCCESS;
 }
 export const LOAD_ORGANISATION_FAILURE = "LOAD_ORGANISATIONS_FAILURE";
-interface LoadOrganisationFailure {
+
+interface LoadOrganisationsFailure {
   type: typeof LOAD_ORGANISATION_FAILURE;
   error: string;
 }
 
-export type LoadOrganisationAction =
-  | LoadOrganisationRequest
-  | LoadOrganisationSuccess
-  | LoadOrganisationFailure;
+export type LoadOrganisationsAction =
+  | LoadOrganisationsRequest
+  | LoadOrganisationsSuccess
+  | LoadOrganisationsFailure;

commit 4182634d3a258e732c34146cc7264e01ea0b1469

fixing type error caused by a refactor in https://github.com/teamsempo/SempoBlockchain/pull/182

app/client/reducers/auth/loginReducer.ts
@@ -18,7 +18,6 @@ interface LoginState {
   error: null | string;
   tfaURL: null | string;
   tfaFailure: boolean;
-  requireTransferCardExists: null | boolean;
   adminTier?: string;
 }
 
@@ -31,7 +30,6 @@ const initialLoginState: LoginState = {
   intercomHash: null,
   webApiVersion: null,
   organisationId: null,
-  requireTransferCardExists: null,
   usdToSatoshiRate: null,
   error: null,
   tfaURL: null,
@@ -58,7 +56,6 @@ export const login = (state = initialLoginState, action: LoginAction) => {
         intercomHash: action.payload.intercomHash,
         webApiVersion: action.payload.webApiVersion,
         organisationId: action.payload.organisationId,
-        requireTransferCardExists: action.payload.requireTransferCardExists,
         email: action.payload.email,
         adminTier: action.payload.adminTier,
         usdToSatoshiRate: action.payload.usdToSatoshiRate,
@@ -74,7 +71,6 @@ export const login = (state = initialLoginState, action: LoginAction) => {
         intercomHash: null,
         webApiVersion: null,
         organisationId: null,
-        requireTransferCardExists: null,
         tfaURL: action.payload.tfaURL,
         tfaFailure: action.payload.tfaFailure,
         error: action.payload.error || "unknown error"

app/client/reducers/auth/types.ts
@@ -32,12 +32,8 @@ export interface LoginSuccessPayload {
   vendorId: null | number;
   intercomHash: null | string;
   webApiVersion: null | string;
-  organisationName: null | string;
   organisationId: number;
-  organisationToken: null | string;
   usdToSatoshiRate: null | number;
-  organisations?: string[];
-  requireTransferCardExists: null | boolean;
   adminTier?: string;
 }
 

app/client/sagas/authSagas.js
@@ -211,7 +211,6 @@ function* refreshToken() {
     yield put(LoginAction.reauthRequest());
     const token_request = yield call(refreshApiToken);
     if (token_request.auth_token) {
-      //todo: investigate this type error
       yield put(
         LoginAction.loginSuccess(createLoginSuccessObject(token_request))
       );