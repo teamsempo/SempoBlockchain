teamsempo/SempoBlockchain

commit cabfa25007dfce27924f6c17dce18dc4022d5f09

Organisations defaults (#182)
* - adding _country_code, _default_disbursement_wei and require_transfer_card to organisation model
- adding ISO 3166-1 alpha-2 country codes

* - adding _country_code, _default_disbursement_wei and require_transfer_card to organisation model
- adding ISO 3166-1 alpha-2 country codes
- updating front-end to normalize organisations

* - removing default disbursement, require transfer card from config
- updating SOPS

* updatin tests

* fixing node build

* adding new configs to org post api

* building organisation PUT api

* adding lat/lng

* frontend

* edit organisation front end

* fixing typescript build

* removing from configs:

default_country
default_lat
default_lng
default_initial_disbursement
require_transfer_card_exists

* fixing migrations

* None type check

* None type check

* tests

* new feature package.json bump

app/client/reducers/auth/loginReducer.ts
@@ -20,16 +20,11 @@ interface LoginState {
   vendorId: null | number;
   intercomHash: null | string;
   webApiVersion: null | string;
-  organisationName: null | string;
-  //TODO(refactor): is it number or string?
   organisationId: null | number;
-  organisationToken: null | string;
   usdToSatoshiRate: null | number;
   error: null | string;
   tfaURL: null | string;
   tfaFailure: boolean;
-  //TODO(refactor): what is this actually?
-  organisations?: null | string[];
   requireTransferCardExists: null | boolean;
   adminTier?: string;
 }
@@ -42,10 +37,7 @@ const initialLoginState: LoginState = {
   vendorId: null,
   intercomHash: null,
   webApiVersion: null,
-  organisationName: null,
   organisationId: null,
-  organisationToken: null,
-  organisations: null,
   requireTransferCardExists: null,
   usdToSatoshiRate: null,
   error: null,
@@ -61,7 +53,6 @@ export const login = (state = initialLoginState, action: LoginAction) => {
     case UPDATE_ACTIVE_ORG:
       return {
         ...state,
-        organisationName: action.payload.organisationName,
         organisationId: action.payload.organisationId
       };
     case LOGIN_SUCCESS:
@@ -73,10 +64,7 @@ export const login = (state = initialLoginState, action: LoginAction) => {
         vendorId: action.vendorId,
         intercomHash: action.intercomHash,
         webApiVersion: action.webApiVersion,
-        organisationName: action.organisationName,
         organisationId: action.organisationId,
-        organisationToken: action.organisationToken,
-        organisations: action.organisations,
         requireTransferCardExists: action.requireTransferCardExists,
         email: action.email,
         adminTier: action.adminTier,
@@ -92,9 +80,7 @@ export const login = (state = initialLoginState, action: LoginAction) => {
         userId: null,
         intercomHash: null,
         webApiVersion: null,
-        organisationName: null,
         organisationId: null,
-        organisations: null,
         requireTransferCardExists: null,
         tfaURL: action.tfaURL,
         tfaFailure: action.tfaFailure,

app/client/reducers/auth/types.ts
@@ -4,9 +4,7 @@ export interface ReauthRequest {
 }
 export const UPDATE_ACTIVE_ORG = "UPDATE_ACTIVE_ORG";
 export interface UpdateActiveOrgPayload {
-  organisationName: string;
   organisationId: number;
-  organisationToken: string;
 }
 interface UpdateActiveOrg {
   type: typeof UPDATE_ACTIVE_ORG;
@@ -40,7 +38,7 @@ export interface LoginSuccess {
   intercomHash: null | string;
   webApiVersion: null | string;
   organisationName: null | string;
-  organisationId: null | number;
+  organisationId: number;
   organisationToken: null | string;
   usdToSatoshiRate: null | number;
   organisations?: string[];

app/client/reducers/organisation/types.ts
@@ -1,32 +1,78 @@
 export interface Organisation {
+  id: number;
   name: string;
   token: {
     symbol: string;
   };
-}
-export const UPDATE_ORGANISATION = "UPDATE_ORGANISATION";
-interface UpdateOrganisation {
-  type: typeof UPDATE_ORGANISATION;
-  organisation: Organisation;
+  require_transfer_card: boolean;
+  default_disbursement: number;
+  country_code: string;
 }
 
-export type OrganisationAction = UpdateOrganisation;
+export const UPDATE_ORGANISATION_LIST = "UPDATE_ORGANISATION_LIST";
+
+interface UpdateOrganisationList {
+  type: typeof UPDATE_ORGANISATION_LIST;
+  organisations: Organisation;
+}
+
+export type OrganisationAction = UpdateOrganisationList;
+
+export const EDIT_ORGANISATION_REQUEST = "EDIT_ORGANISATION_REQUEST";
+
+export interface EditOrganisationPayload {
+  body: {
+    country_code: string;
+    default_disbursement: number;
+    require_transfer_card: boolean;
+    default_lat: number;
+    default_lng: number;
+  };
+  path: number;
+}
+
+interface EditOrganisationRequest {
+  type: typeof EDIT_ORGANISATION_REQUEST;
+  payload: EditOrganisationPayload;
+}
+
+export const EDIT_ORGANISATION_SUCCESS = "EDIT_ORGANISATION_SUCCESS";
+
+interface EditOrganisationSuccess {
+  type: typeof EDIT_ORGANISATION_SUCCESS;
+}
+
+export const EDIT_ORGANISATION_FAILURE = "EDIT_ORGANISATION_FAILURE";
+
+interface EditOrganisationFailure {
+  type: typeof EDIT_ORGANISATION_FAILURE;
+  error: string;
+}
+
+export type EditOrganisationAction =
+  | EditOrganisationRequest
+  | EditOrganisationSuccess
+  | EditOrganisationFailure;
+
 
 export const LOAD_ORGANISATION_REQUEST = "LOAD_ORGANISATIONS_REQUEST";
-interface LoadOrganisationRequest {
+
+interface LoadOrganisationsRequest {
   type: typeof LOAD_ORGANISATION_REQUEST;
 }
 export const LOAD_ORGANISATION_SUCCESS = "LOAD_ORGANISATIONS_SUCCESS";
-interface LoadOrganisationSuccess {
+
+interface LoadOrganisationsSuccess {
   type: typeof LOAD_ORGANISATION_SUCCESS;
 }
 export const LOAD_ORGANISATION_FAILURE = "LOAD_ORGANISATIONS_FAILURE";
-interface LoadOrganisationFailure {
+
+interface LoadOrganisationsFailure {
   type: typeof LOAD_ORGANISATION_FAILURE;
   error: string;
 }
 
-export type LoadOrganisationAction =
-  | LoadOrganisationRequest
-  | LoadOrganisationSuccess
-  | LoadOrganisationFailure;
+export type LoadOrganisationsAction =
+  | LoadOrganisationsRequest
+  | LoadOrganisationsSuccess
+  | LoadOrganisationsFailure;

commit 0b570d6f67f788ec39a0ec2cca92f4bbf433b603

frontend tests: “Zero to Some” (#210)
* * fix LoginActions bug
* started writing utils.js tests

* - utils.test.js
- adding local storage mock

* updating redux-saga to latest

* bump bump

* refactoring to mocks

* bump

* fix for https://github.com/teamsempo/SempoBlockchain/issues/224

* fix for https://github.com/teamsempo/SempoBlockchain/issues/224

* fixing bug

* fixing bug

* fixing type error
caused by a refactor in https://github.com/teamsempo/SempoBlockchain/pull/182

Co-authored-by: Nick Williams <enjeyw@gmail.com>

app/client/__mocks__/local-storage-mock.js
@@ -1,0 +1,23 @@
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();

app/client/__mocks__/redux-mock-store.js
@@ -1,0 +1,10 @@
+import configureMockStore from "redux-mock-store";
+
+const middlewares = [];
+const mockStore = configureMockStore(middlewares);
+
+// Initialize mockstore with empty state
+const initialState = { login: { webApiVersion: 1 } };
+const store = mockStore(initialState);
+
+export default store;

app/client/app.jsx
@@ -2,48 +2,11 @@ import "babel-polyfill";
 import "react-dates/initialize";
 
 import React from "react";
-import { createStore, applyMiddleware, compose } from "redux";
-import { connect, Provider } from "react-redux";
-import createSagaMiddleware from "redux-saga";
-import { delayConfiguration } from "pusher-redux";
-import * as Sentry from "@sentry/browser";
+import { Provider } from "react-redux";
 import { createBrowserHistory } from "history";
-import { version } from "../package.json";
 
-import appReducer from "./reducers/rootReducer";
-import rootsaga from "./sagas/rootSaga";
-
+import store from "./createStore.js";
 import Nav from "./nav.jsx";
-
-const sagaMiddleware = createSagaMiddleware();
-
-// Setup redux dev tools
-const composeSetup =
-  process.env.NODE_ENV !== "prod" &&
-  typeof window === "object" &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : compose;
-
-export const store = createStore(
-  appReducer,
-  composeSetup(applyMiddleware(sagaMiddleware))
-);
-
-// Setup sentry
-Sentry.init({
-  dsn: window.SENTRY_REACT_DSN,
-  release: "sempo-blockchain-react@" + version
-});
-
-// Pusher Options
-const pusherOptions = {
-  cluster: "ap1"
-};
-
-delayConfiguration(store, window.PUSHER_KEY, pusherOptions);
-
-sagaMiddleware.run(rootsaga);
 
 export const browserHistory = createBrowserHistory({});
 

app/client/createStore.js
@@ -1,0 +1,40 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { delayConfiguration } from "pusher-redux";
+import * as Sentry from "@sentry/browser";
+import { createBrowserHistory } from "history";
+import { version } from "../package.json";
+
+import appReducer from "./reducers/rootReducer";
+import rootSaga from "./sagas/rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+// Setup redux dev tools
+const composeSetup =
+  process.env.NODE_ENV !== "prod" &&
+  typeof window === "object" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
+const store = createStore(
+  appReducer,
+  composeSetup(applyMiddleware(sagaMiddleware))
+);
+export default store;
+
+// Setup sentry
+Sentry.init({
+  dsn: window.SENTRY_REACT_DSN,
+  release: "sempo-blockchain-react@" + version
+});
+
+// Pusher Options
+const pusherOptions = {
+  cluster: "ap1"
+};
+
+delayConfiguration(store, window.PUSHER_KEY, pusherOptions);
+
+sagaMiddleware.run(rootSaga);

app/client/reducers/auth/loginReducer.ts
@@ -18,7 +18,6 @@ interface LoginState {
   error: null | string;
   tfaURL: null | string;
   tfaFailure: boolean;
-  requireTransferCardExists: null | boolean;
   adminTier?: string;
 }
 
@@ -31,7 +30,6 @@ const initialLoginState: LoginState = {
   intercomHash: null,
   webApiVersion: null,
   organisationId: null,
-  requireTransferCardExists: null,
   usdToSatoshiRate: null,
   error: null,
   tfaURL: null,
@@ -58,7 +56,6 @@ export const login = (state = initialLoginState, action: LoginAction) => {
         intercomHash: action.payload.intercomHash,
         webApiVersion: action.payload.webApiVersion,
         organisationId: action.payload.organisationId,
-        requireTransferCardExists: action.payload.requireTransferCardExists,
         email: action.payload.email,
         adminTier: action.payload.adminTier,
         usdToSatoshiRate: action.payload.usdToSatoshiRate,
@@ -74,7 +71,6 @@ export const login = (state = initialLoginState, action: LoginAction) => {
         intercomHash: null,
         webApiVersion: null,
         organisationId: null,
-        requireTransferCardExists: null,
         tfaURL: action.payload.tfaURL,
         tfaFailure: action.payload.tfaFailure,
         error: action.payload.error || "unknown error"

app/client/reducers/auth/types.ts
@@ -32,12 +32,8 @@ export interface LoginSuccessPayload {
   vendorId: null | number;
   intercomHash: null | string;
   webApiVersion: null | string;
-  organisationName: null | string;
   organisationId: number;
-  organisationToken: null | string;
   usdToSatoshiRate: null | number;
-  organisations?: string[];
-  requireTransferCardExists: null | boolean;
   adminTier?: string;
 }
 

app/client/sagas/messageSaga.js
@@ -1,5 +1,5 @@
 import { delay } from "redux-saga";
-import { call, put, select, take, takeEvery } from "redux-saga/effects";
+import { call, put, select, take, takeEvery, all } from "redux-saga/effects";
 
 import {
   ADD_FLASH_MESSAGE,
@@ -38,6 +38,10 @@ export function* onAddFlashMessage() {
   }
 }
 
-export default function* watchOnAddFlashMessage() {
+function* watchOnAddFlashMessage() {
   yield takeEvery(ADD_FLASH_MESSAGE, onAddFlashMessage);
+}
+
+export default function* messageSagas() {
+  yield all([watchOnAddFlashMessage()]);
 }

app/client/sagas/rootSaga.js
@@ -6,7 +6,7 @@ import credit_transferSagas from "./creditTransferSagas";
 import transferAccountSagas from "./transferAccountSagas";
 import newExportSaga from "./exportSaga";
 import userSagas from "./userSagas";
-import watchOnAddFlashMessage from "./messageSaga";
+import messageSagas from "./messageSaga";
 import filterSagas from "./filterSaga";
 import businessVerificationSaga from "./businessVerificationSaga";
 import wyreSaga from "./wyreSaga";
@@ -20,7 +20,7 @@ export default function* rootSaga() {
     authSagas(),
     spreadsheetSagas(),
     credit_transferSagas(),
-    watchOnAddFlashMessage(),
+    messageSagas(),
     transferAccountSagas(),
     newExportSaga(),
     userSagas(),

app/client/sagas/transferUsageSagas.js
@@ -31,8 +31,6 @@ function* loadTransferUsages({ payload }) {
   } catch (fetch_error) {
     const error = yield call(handleError, fetch_error);
 
-    console.log("error is:", error);
-
     yield put({ type: LOAD_TRANSFER_USAGES_FAILURE, error: error });
 
     yield put({ type: ADD_FLASH_MESSAGE, error: true, message: error.message });

app/client/sagas/userSagas.js
@@ -34,7 +34,6 @@ import {
   RESET_PIN_REQUEST,
   RESET_PIN_SUCCESS
 } from "../reducers/userReducer";
-import { LOAD_TRANSFER_USAGES_REQUEST } from "../reducers/transferUsage/types";
 import { UPDATE_TRANSFER_ACCOUNTS } from "../reducers/transferAccountReducer";
 import { browserHistory } from "../app";
 
@@ -179,8 +178,6 @@ function* createUser({ payload }) {
     yield call(updateStateFromUser, result.data);
 
     yield put({ type: CREATE_USER_SUCCESS, result });
-
-    yield put({ type: LOAD_TRANSFER_USAGES_REQUEST });
   } catch (fetch_error) {
     const error = yield call(handleError, fetch_error);
 

app/client/utils.js
@@ -1,8 +1,7 @@
-import { call } from "redux-saga/effects";
+import { call, put } from "redux-saga/effects";
 import merge from "deepmerge";
-import { LOGIN_FAILURE } from "./reducers/auth/types";
-import { put } from "redux-saga/es/effects";
-import { store } from "./app.jsx";
+import { LoginAction } from "./reducers/auth/actions";
+import store from "./createStore.js";
 import { USER_FILTER_TYPE } from "./constants";
 
 export function formatMoney(
@@ -92,7 +91,15 @@ export const parseQuery = queryString => {
 
 export const generateFormattedURL = (url, query, path) => {
   let URL;
-  let version = store.getState().login.webApiVersion;
+  let version;
+
+  try {
+    version = store.getState().login.webApiVersion;
+  } catch (e) {
+    console.log("Something went wrong", e);
+    version = null;
+  }
+
   if (!version) {
     version = "1"; // fallback to V1 API
   }
@@ -127,7 +134,7 @@ export function* handleError(error) {
   }
 
   if (error.status === 401) {
-    yield put({ type: LOGIN_FAILURE, error: message });
+    yield put(LoginAction.loginFailure(message));
   }
 
   return { message, status };

app/client/__tests__/setup/setupTests.js
@@ -1,4 +1,10 @@
+import "isomorphic-fetch";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+import "../../__mocks__/local-storage-mock.js";
+import mockStore from "../../__mocks__/redux-mock-store.js";
+
+jest.mock("../../createStore.js", () => mockStore);

app/client/__tests__/utils.test.js
@@ -1,0 +1,58 @@
+import "./setup/setupTests";
+import * as utils from "../utils";
+
+describe("test formatMoney", () => {
+  it.each([
+    [200, undefined, undefined, undefined, "AUD", "200.00 AUD"],
+    [-200, undefined, undefined, undefined, "AUD", "-200.00 AUD"],
+    [undefined, undefined, undefined, undefined, "AUD", "0.00 AUD"],
+    [2, 1, undefined, undefined, "USD", "2.0 USD"],
+    [999999, 4, ".", ",", "R", "999,999.0000 R"]
+  ])(
+    ".formatMoney(%i, %i, %s, %s, %s) == %s",
+    (amount, decimalCount, decimal, thousands, currency, expected) => {
+      expect(
+        utils.formatMoney(amount, decimalCount, decimal, thousands, currency)
+      ).toBe(expected);
+    }
+  );
+});
+
+describe("test URL functions", () => {
+  test("test parseQuery", () => {
+    expect(utils.parseQuery("?is_deleted=true&foo=bar")).toStrictEqual({
+      foo: "bar",
+      is_deleted: "true"
+    });
+  });
+
+  test("test generateFormattedURL", () => {
+    expect(
+      utils.generateFormattedURL("/user/", { is_deleted: true, foo: "bar" }, 1)
+    ).toBe("/api/v1/user/?is_deleted=true&foo=bar");
+  });
+});
+
+describe("test localStorage", () => {
+  test("test setOrgId", () => {
+    utils.storeOrgid("1");
+    expect(localStorage).toHaveProperty("orgId", "1");
+  });
+
+  test("test getOrgId", () => {
+    expect(utils.getOrgId()).toEqual("1");
+  });
+
+  test("test removeOrgId", () => {
+    utils.removeOrgId("1");
+    expect(localStorage).not.toHaveProperty("orgId", "1");
+  });
+});
+
+test("replaceUnderscores", () => {
+  expect(utils.replaceUnderscores("Hello_World")).toBe("Hello World");
+});
+
+test("replaceSpaces", () => {
+  expect(utils.replaceSpaces("Hello World")).toBe("Hello-World");
+});