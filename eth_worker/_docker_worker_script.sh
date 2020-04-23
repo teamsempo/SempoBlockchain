77249ebf167c2361a804533db87d85a93d83ed1e
michielderoos <michiel@deroos.ca>
2020-04-23 16:46:26 -0300

    Run alembic upgrade only once

--- eth_worker/_docker_worker_script.sh
+++ eth_worker/_docker_worker_script.sh
@@ -15,6 +15,7 @@ elif [ "$CONTAINER_TYPE" == 'FILTER' ]; then
   python ethereum_filter_test.py
 elif [ "$CONTAINER_TYPE" == 'PROCESSOR' ]; then
   echo "Starting Processor Worker"
+  alembic upgrade head
   celery -A eth_manager worker --loglevel=INFO --concurrency=$WORKER_CONCURRENCY --pool=eventlet -Q=processor --without-gossip --without-mingle
 elif [ "$CONTAINER_TYPE" == 'LOW_PRIORITY_WORKER' ]; then
   echo "Starting Low Priority Worker"
@@ -34,6 +35,7 @@ else
     exit $ret
   fi
   echo "Starting Generic Worker (Default)"
+  alembic upgrade head
   celery -A eth_manager worker --loglevel=INFO --concurrency=10 --pool=eventlet -Q=low-priority,celery,high-priority --without-gossip --without-mingle
 fi
 
