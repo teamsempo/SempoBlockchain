82085cedcc5c4f334121c67ac6c1df3dd22e7c9d
tristanhcole <tristanhcole@gmail.com>
2020-04-29 14:08:56 +1000

    fix

--- devtools/prepare_aws_deploy.sh
+++ devtools/prepare_aws_deploy.sh
@@ -6,6 +6,8 @@ bash build_docker.sh
 
 GIT_HASH=$(git rev-parse HEAD)
 
+cd ../
+
 sed "s|REPOSITORY_URI|$REPOSITORY_URI|g; s|TAG_SUFFIX|$GIT_HASH|g" awsDockerrunTemplate.json > Dockerrun.aws.json
 
 eval $(aws ecr get-login --no-include-email --region $ECR_REGION --profile $ECR_BUILDER);
