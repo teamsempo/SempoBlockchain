d39ea0c1102af41f9464d7d7f6ba26f77b0fdd7e
Nick Williams <enjeyw@gmail.com>
2020-05-01 16:54:18 +1000

    curious to test threading on dev!

--- app/server/utils/amazon_ses.py
+++ app/server/utils/amazon_ses.py
@@ -109,6 +109,10 @@ def get_email_template(TEMPLATE_FILE):
 @executor.job
 def ses_email_handler(recipient, subject, textbody, htmlbody = None):
     sender = "admin@withsempo.com"
+
+    from time import sleep
+    sleep(90)
+
     awsregion = "us-west-2"
     htmlbody = htmlbody or textbody
     charset = "UTF-8"
