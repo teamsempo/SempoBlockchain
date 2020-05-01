8db356157df2779498203b8e2c791f4ce4817237
Nick Williams <enjeyw@gmail.com>
2020-05-01 16:06:02 +1000

    path fixes

--- devtools/prepare_aws_deploy.sh
+++ devtools/prepare_aws_deploy.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-bash build_docker.sh
-
-# REPOSITORY_URI=290492953667.dkr.ecr.ap-southeast-2.amazonaws.com/blockchaindemo
+#bash build_docker.sh
 
 GIT_HASH=$(git rev-parse HEAD)
 
 cd ../
 
-sed "s|REPOSITORY_URI|$REPOSITORY_URI|g; s|TAG_SUFFIX|$GIT_HASH|g" awsDockerrunTemplate.json > Dockerrun.aws.json
+sed "s|REPOSITORY_URI|$REPOSITORY_URI|g; s|TAG_SUFFIX|$GIT_HASH|g" ./awsDockerrunTemplate.json > Dockerrun.aws.json
 
 eval $(aws ecr get-login --no-include-email --region $ECR_REGION --profile $ECR_BUILDER);
 
