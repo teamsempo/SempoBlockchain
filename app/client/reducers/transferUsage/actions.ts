2e790ef6e95dfe8391aa20ac985becedf4809df3
Tristan Cole <tristanhcole@gmail.com>
2020-05-01 15:24:58 +1000

    typescriptifying (#228)
    * bump
    
    * WIP
    
    * refactoring message reducer
    
    * WIP
    
    * user reducer to typescript
    
    * deleting old user reducer
    
    * updating organisation reducer to use generics
    
    * updating transfer usages reducer to use generics
    
    * refactoring export reducer to typescript
    refactoring export api to standard format/error handling
    
    * bump
    
    * bump
    
    * fix
    
    * version bump
    
    Co-authored-by: Nick Williams <enjeyw@gmail.com>

--- app/client/reducers/transferUsage/actions.ts
+++ app/client/reducers/transferUsage/actions.ts
@@ -1,12 +1,34 @@
 import {
-  LOAD_TRANSFER_USAGES_REQUEST,
   LoadTransferUsagePayload,
-  LoadTransferUsagesAction
+  TransferUsage,
+  TransferUsageActionTypes,
+  LoadTransferUsagesActionTypes
 } from "./types";
+import { createAction, ActionsUnion } from "../../reduxUtils";
 
-export const loadTransferUsages = (
-  payload: LoadTransferUsagePayload
-): LoadTransferUsagesAction => ({
-  type: LOAD_TRANSFER_USAGES_REQUEST,
-  payload
-});
+export const TransferUsageAction = {
+  updateTransferUsages: (transferUsages: TransferUsage[]) =>
+    createAction(
+      TransferUsageActionTypes.UPDATE_TRANSFER_USAGES,
+      transferUsages
+    )
+};
+export type TransferUsageAction = ActionsUnion<typeof TransferUsageAction>;
+
+export const LoadTransferUsagesAction = {
+  loadTransferUsagesRequest: (payload: LoadTransferUsagePayload) =>
+    createAction(
+      LoadTransferUsagesActionTypes.LOAD_TRANSFER_USAGES_REQUEST,
+      payload
+    ),
+  loadTransferUsagesSuccess: () =>
+    createAction(LoadTransferUsagesActionTypes.LOAD_TRANSFER_USAGES_SUCCESS),
+  loadTransferUsagesFailure: (error: string) =>
+    createAction(
+      LoadTransferUsagesActionTypes.LOAD_TRANSFER_USAGES_FAILURE,
+      error
+    )
+};
+export type LoadTransferUsagesAction = ActionsUnion<
+  typeof LoadTransferUsagesAction
+>;
--- app/client/reducers/transferUsage/types.ts
+++ app/client/reducers/transferUsage/types.ts
@@ -2,33 +2,19 @@ export interface TransferUsage {
   id: string;
   name: string;
 }
-export const UPDATE_TRANSFER_USAGES = "UPDATE_TRANSFER_USAGES";
-interface UpdateTransferUsages {
-  type: typeof UPDATE_TRANSFER_USAGES;
-  transferUsages: TransferUsage[];
-}
-export type TransferUsagesAction = UpdateTransferUsages;
 
-export const LOAD_TRANSFER_USAGES_REQUEST = "LOAD_TRANSFER_USAGES_REQUEST";
+export enum TransferUsageActionTypes {
+  UPDATE_TRANSFER_USAGES = "UPDATE_TRANSFER_USAGES"
+}
+
 export interface LoadTransferUsagePayload {
   query?: {
     show_all?: boolean;
   };
 }
-interface LoadTransferUsagesRequest {
-  type: typeof LOAD_TRANSFER_USAGES_REQUEST;
-  payload: LoadTransferUsagePayload;
+
+export enum LoadTransferUsagesActionTypes {
+  LOAD_TRANSFER_USAGES_REQUEST = "LOAD_TRANSFER_USAGES_REQUEST",
+  LOAD_TRANSFER_USAGES_SUCCESS = "LOAD_TRANSFER_USAGES_SUCCESS",
+  LOAD_TRANSFER_USAGES_FAILURE = "LOAD_TRANSFER_USAGES_FAILURE"
 }
-export const LOAD_TRANSFER_USAGES_SUCCESS = "LOAD_TRANSFER_USAGES_SUCCESS";
-interface LoadTransferUsagesSuccess {
-  type: typeof LOAD_TRANSFER_USAGES_SUCCESS;
-}
-export const LOAD_TRANSFER_USAGES_FAILURE = "LOAD_TRANSFER_USAGES_FAILURE";
-interface LoadTransferUsagesFailure {
-  type: typeof LOAD_TRANSFER_USAGES_FAILURE;
-  error: string;
-}
-export type LoadTransferUsagesAction =
-  | LoadTransferUsagesRequest
-  | LoadTransferUsagesSuccess
-  | LoadTransferUsagesFailure;
