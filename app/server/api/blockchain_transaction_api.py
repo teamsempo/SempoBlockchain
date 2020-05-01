9f63348b29e1ebe9ab5b6a7ac23ee4d93d744be0
Michiel <michiel@deroos.ca>
2020-05-01 11:42:00 -0300

    Nix Unused Blockchain Code (#238)
    * with lock
    
    * Nix BlockchainTransaction from server
    
    * DB migration also
    
    * merge heads
    
    Co-authored-by: Nick Williams <enjeyw@gmail.com>

--- app/server/api/blockchain_transaction_api.py
+++ app/server/api/blockchain_transaction_api.py
@@ -3,78 +3,9 @@ from flask.views import MethodView
 
 from server import db, bt
 from server.models.credit_transfer import CreditTransfer
-from server.models.blockchain_transaction import BlockchainTransaction
-from server.utils.blockchain_transaction import add_full_transaction_details, claim_nonce
 from server.utils.auth import requires_auth
 
 blockchain_transaction_blueprint = Blueprint('make_blockchain_transaction', __name__)
-
-class BlockchainTransactionAPI(MethodView):
-
-    @requires_auth(allowed_basic_auth_types=('internal'))
-    def get(self):
-        """
-        :return: list of bitcoin transactions with outputs that have not yet been identified as spent
-        """
-        txns_with_unspent = (BlockchainTransaction.query
-                             .filter_by(is_bitcoin=True)
-                             .filter_by(status='SUCCESS')
-                             .filter_by(has_output_txn=False)
-                             .all())
-
-        txn_dict = {}
-
-        for txn in txns_with_unspent:
-            # Has the txn come from an internal user that we should be watching?
-            if txn.credit_transfer.recipient_transfer_account:
-
-                hash = txn.hash
-                recipient_add = txn.credit_transfer.recipient_transfer_account.blockchain_address.address
-                txn_id = txn.id
-
-                if hash in txn_dict:
-                    txn_dict[hash][recipient_add] = txn_id
-                else:
-                    txn_dict[hash] = {recipient_add: txn_id}
-
-        return make_response(jsonify({'transactions': txn_dict})), 201
-
-
-    @requires_auth(allowed_basic_auth_types=('internal'))
-    def post(self):
-
-        post_data = request.get_json()
-
-        if post_data.get('is_bitcoin'):
-            response, status_code = add_full_transaction_details(
-                post_data,
-                method='POST',
-                force_transaction_creation=post_data.get('force_transaction_creation')
-            )
-        else:
-            response, status_code = claim_nonce(post_data)
-
-        if status_code == 201:
-            db.session.commit()
-
-        return response, status_code
-
-
-    @requires_auth(allowed_basic_auth_types=('internal'))
-    def put(self):
-        put_data = request.get_json()
-
-        response, status_code = add_full_transaction_details(
-            put_data,
-            method='PUT',
-            force_transaction_creation=put_data.get('force_transaction_creation')
-        )
-
-        if status_code == 201:
-            db.session.commit()
-
-        return response, status_code
-
 
 class BlockchainTransactionRPC(MethodView):
 
@@ -129,13 +60,6 @@ class BlockchainTransactionRPC(MethodView):
         }
 
         return make_response(jsonify(response_object)), 400
-
-
-blockchain_transaction_blueprint.add_url_rule(
-    '/blockchain_transaction/',
-    view_func=BlockchainTransactionAPI.as_view('blockchain_transaction_view'),
-    methods=['GET', 'POST', 'PUT']
-)
 
 blockchain_transaction_blueprint.add_url_rule(
     '/blockchain_transaction_rpc/',
