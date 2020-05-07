bceb872e56d7fe0ebe58ade11baacaf9fb932ff0
michielderoos <michiel@deroos.ca>
2020-05-07 17:02:13 -0300

    merge heads

---
+++ app/migrations/versions/eced23bc3d13_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: eced23bc3d13
+Revises: 984d7e9c50af, d0f42c2a8483
+Create Date: 2020-05-07 17:01:46.063016
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eced23bc3d13'
+down_revision = ('984d7e9c50af', 'd0f42c2a8483')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
