20b03dc474407a03cb2ed75e7f0dce19d38364c3
Nick Williams <enjeyw@gmail.com>
2020-05-20 12:05:57 +1000

    hopefully tests running on eth wokrer

--- app/_docker_app_script.sh
+++ app/_docker_app_script.sh
@@ -17,13 +17,15 @@ if [ "$ret" -ne 0 ]; then
   exit $ret
 fi
 
-if [ "$CONTAINER_MODE" = 'TEST' ]; then
-   coverage pytest
+if [ "$CONTAINER_MODE" == 'TEST' ]; then
+   coverage run -m pytest
    ret=$?
    if [ "$ret" -ne 0 ]; then
      exit $ret
    fi
    bash <(curl -s https://codecov.io/bash) -cF python
+elif [ "$CONTAINER_MODE" == 'ETH_WORKER_TEST' ]; then
+   echo pass
 else
   uwsgi --socket 0.0.0.0:9000 --protocol http  --processes 4 --enable-threads --module=server.wsgi:app --stats :3031 --stats-http
 fi
--- eth_worker/_docker_worker_script.sh
+++ eth_worker/_docker_worker_script.sh
@@ -5,46 +5,51 @@ WORKER_CONCURRENCY=4
 
 echo "Running docker worker script"
 
-if [ "$CONTAINER_TYPE" == 'BEAT' ]; then
-  echo "Starting Beat Worker"
-  celery -A eth_manager beat --loglevel=WARNING
-elif [ "$CONTAINER_TYPE" == 'FILTER' ]; then
-  python ethereum_filter_test.py
-elif [ "$CONTAINER_TYPE" == 'PROCESSOR' ]; then
-  echo "Starting Processor Worker"
-  celery -A eth_manager worker --loglevel=INFO --concurrency=$WORKER_CONCURRENCY --pool=eventlet -Q=processor --without-gossip --without-mingle
-elif [ "$CONTAINER_TYPE" == 'LOW_PRIORITY_WORKER' ]; then
-  echo "Starting Low Priority Worker"
-  celery -A eth_manager worker --loglevel=INFO --concurrency=$WORKER_CONCURRENCY --pool=eventlet -Q=low-priority,celery --without-gossip --without-mingle
-elif [ "$CONTAINER_TYPE" == 'HIGH_PRIORITY_WORKER' ]; then
-  echo "Starting High Priority Worker"
-  celery -A eth_manager worker --loglevel=INFO --concurrency=$WORKER_CONCURRENCY --pool=eventlet -Q=high-priority --without-gossip --without-mingle
-elif [ "$CONTAINER_TYPE" == 'FLOWER' ]; then
-  flower -A worker --port=5555
-elif [ "$CONTAINER_TYPE" == 'ANY_PRIORITY_WORKER' ]; then
-  echo "Starting Any Priority Worker"
-  celery -A eth_manager worker --loglevel=INFO --concurrency=$WORKER_CONCURRENCY --pool=eventlet -Q=low-priority,celery,high-priority,processor --without-gossip --without-mingle
-
-else
-  echo "Running alembic upgrade (Default)"
-  alembic upgrade head
-  ret=$?
-  if [ "$ret" -ne 0 ]; then
-    exit $ret
-  fi
-
-  if [ "$CONTAINER_MODE" = 'ETH_WORKER_TEST' ]; then
-    coverage pytest
-    ret=$?
-    if [ "$ret" -ne 0 ]; then
-      exit $ret
+if [ "$CONTAINER_MODE" == 'ETH_WORKER_TEST' ]; then
+    if [ "$CONTAINER_TYPE" == 'PRIMARY' ]; then
+        echo "Running alembic upgrade (Default)"
+        alembic upgrade head
+        ret=$?
+        if [ "$ret" -ne 0 ]; then
+            exit $ret
+        fi
+        coverage run -m pytest
+        ret=$?
+        if [ "$ret" -ne 0 ]; then
+          exit $ret
+        fi
+        bash <(curl -s https://codecov.io/bash) -cF python
+    else
+        echo pass
     fi
-    bash <(curl -s https://codecov.io/bash) -cF python
-  else
-    echo "Starting Generic Worker (Default)"
-    celery -A eth_manager worker --loglevel=INFO --concurrency=10 --pool=eventlet -Q=low-priority,celery,high-priority --without-gossip --without-mingle
-  fi
+else
+    if [ "$CONTAINER_TYPE" == 'BEAT' ]; then
+        echo "Starting Beat Worker"
+        celery -A eth_manager beat --loglevel=WARNING
+    elif [ "$CONTAINER_TYPE" == 'FLOWER' ]; then
+        echo "Starting Flower Worker"
+        flower -A worker --port=5555
+    elif [ "$CONTAINER_TYPE" == 'PROCESSOR' ]; then
+        echo "Starting Processor Worker"
+        celery -A eth_manager worker --loglevel=INFO --concurrency=$WORKER_CONCURRENCY --pool=eventlet -Q=processor --without-gossip --without-mingle
+    elif [ "$CONTAINER_TYPE" == 'LOW_PRIORITY_WORKER' ]; then
+        echo "Starting Low Priority Worker"
+        celery -A eth_manager worker --loglevel=INFO --concurrency=$WORKER_CONCURRENCY --pool=eventlet -Q=low-priority,celery --without-gossip --without-mingle
+    elif [ "$CONTAINER_TYPE" == 'HIGH_PRIORITY_WORKER' ]; then
+        echo "Starting High Priority Worker"
+        celery -A eth_manager worker --loglevel=INFO --concurrency=$WORKER_CONCURRENCY --pool=eventlet -Q=high-priority --without-gossip --without-mingle
+    elif [ "$CONTAINER_TYPE" == 'ANY_PRIORITY_WORKER' ]; then
+        echo "Starting Any Priority Worker"
+        celery -A eth_manager worker --loglevel=INFO --concurrency=$WORKER_CONCURRENCY --pool=eventlet -Q=low-priority,celery,high-priority,processor --without-gossip --without-mingle
+    else
+#       Default to primary worker configuration - running alembic upgrade twice is fine.
+        echo "Running alembic upgrade (Default)"
+        alembic upgrade head
+        ret=$?
+        if [ "$ret" -ne 0 ]; then
+            exit $ret
+        fi
+        echo "Starting Generic Worker (Default)"
+        celery -A eth_manager worker --loglevel=INFO --concurrency=10 --pool=eventlet -Q=low-priority,celery,high-priority --without-gossip --without-mingle
+    fi
 fi
-
-#
-#celery -A worker beat --loglevel=WARNING
