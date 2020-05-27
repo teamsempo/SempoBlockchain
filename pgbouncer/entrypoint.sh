150d3b7d3987b5c90ccb3cfcf8f4e4dbcdf36a1b
Nick Williams <enjeyw@gmail.com>
2020-05-27 12:20:18 +1000

    logging config contents

--- pgbouncer/entrypoint.sh
+++ pgbouncer/entrypoint.sh
@@ -4,4 +4,8 @@
 echo "Running pgbouncer config generator"
 python3 -u generate_pgbouncer_config.py
 echo "Generated sempo pgboucner file. Starting pgbouncer"
+
+echo "config contents:"
+cat /etc/pgbouncer/pgbouncer.ini
+
 exec "$@"
