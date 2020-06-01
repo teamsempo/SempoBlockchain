90558bf290e994ad790a1d4ce60482c05503dd12
Nick Williams <enjeyw@gmail.com>
2020-06-01 22:59:36 +1000

    adding alternative test

--- app/test_app/functional/api/test_user_api.py
+++ app/test_app/functional/api/test_user_api.py
@@ -5,10 +5,26 @@ from faker import Faker
 from server.utils.auth import get_complete_auth_token
 from server.utils.phone import proccess_phone_number
 from server.models.transfer_usage import TransferUsage
-from server.models.user import User
+from server.utils.location import async_set_user_gps_from_location
 
 fake = Faker()
 fake.add_provider(phone_number)
+
+
+@pytest.fixture(scope='function')
+def mock_async_set_user_gps_from_location(mocker):
+    # Always patch out all sms sending apis because we don't want to spam messages with our tests!!
+    fn_inputs = []
+
+    class Mock:
+
+        @staticmethod
+        def submit(*args, **kwargs):
+            fn_inputs.append([args, kwargs])
+
+    mocker.patch('server.utils.location.async_set_user_gps_from_location', Mock)
+
+    return fn_inputs
 
 
 @pytest.mark.parametrize("user_phone_accessor, phone, business_usage_name, referred_by, tier, status_code", [
@@ -16,8 +32,10 @@ fake.add_provider(phone_number)
     (lambda o: o.phone, fake.msisdn(), 'Fuel/Energy', fake.msisdn(), 'superadmin', 200),
     (lambda o: o.phone, fake.msisdn(), 'Food/Water', fake.msisdn(), 'view', 403)
 ])
-def test_create_user(test_client, authed_sempo_admin_user, init_database, create_transfer_account_user, user_phone_accessor, phone,
-                     business_usage_name, referred_by, tier, status_code):
+def test_create_user(test_client, authed_sempo_admin_user, init_database, create_transfer_account_user,
+                     mock_async_set_user_gps_from_location,
+                     user_phone_accessor, phone, business_usage_name, referred_by, tier, status_code):
+
     if tier:
         authed_sempo_admin_user.set_held_role('ADMIN', tier)
         auth = get_complete_auth_token(authed_sempo_admin_user)
@@ -66,6 +84,12 @@ def test_create_user(test_client, authed_sempo_admin_user, init_database, create
         assert data['user']['business_usage_id'] == init_database.session.query(TransferUsage)\
             .filter_by(name=business_usage_name).first().id
         assert data['user']['referred_by'] == user_phone_accessor(create_transfer_account_user)
+
+        # Checks that we're calling the gps location fetching job, and passing the right data to it
+        # Used in lieu of the test below working
+        fn_inputs = mock_async_set_user_gps_from_location
+        args, kwargs = fn_inputs[-1]
+        assert args == (data['user']['id'], 'Elwood')
 
         # TODO: Work out why the latlng remains none even though it definitely makes it into db
         # # Done async, so sleep to prevent race on this check
