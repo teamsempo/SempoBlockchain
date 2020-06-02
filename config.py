c6981703859e8fab3df260e08a44104571f5313a
michielderoos <michiel@deroos.ca>
2020-06-02 19:19:47 -0300

    version++

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=env_loglevel)
 logg = logging.getLogger(__name__)
 
 
-VERSION = '1.1.37'  # Remember to bump this in every PR
+VERSION = '1.1.38'  # Remember to bump this in every PR
 
 logg.info('Loading configs at UTC {}'.format(datetime.datetime.utcnow()))
 
