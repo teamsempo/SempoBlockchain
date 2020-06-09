abf19f69cb77a70ba8b5ab230488709b8d3579cc
michielderoos <michiel@deroos.ca>
2020-06-09 11:41:06 -0300

    Merge heads

---
+++ eth_worker/migrations/versions/00c29bd2df26_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 00c29bd2df26
+Revises: 4e492f7dba64
+Create Date: 2020-06-09 11:40:52.338349
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '00c29bd2df26'
+down_revision = '4e492f7dba64'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
