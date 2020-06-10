716a8225d7608c0bbd953bcff58156860311797d
Greg Taschuk <gregorytaschuk@gmail.com>
2020-06-10 16:46:02 -0700

    added test for create_function_task

--- eth_worker/test_eth_worker/unit/test_sql_persistence.py
+++ eth_worker/test_eth_worker/unit/test_sql_persistence.py
@@ -202,6 +202,32 @@ class TestInterface:
 
         assert trans.task.uuid == task.uuid
 
+    def test_create_function_task(self, db_session, persistence_int: SQLPersistenceInterface):
+
+        signing_wallet_obj = BlockchainWallet()
+        db_session.add(signing_wallet_obj)
+
+        uuid = str_uuid()
+        contract_address = '0x1234'
+        abi_type = "ERC20" # aka contract_type
+        function_name = "transferFrom"
+        args = ['0x1234', '0x2345', int(1e36)]
+        kwargs = None
+        signing_address = None
+        encrypted_private_key = None
+        gas_limit = None
+        prior_tasks = None
+        reserves_task = None
+
+        trans = persistence_int.create_function_task(uuid,
+                signing_wallet_obj,
+                contract_address, abi_type,
+                function_name, args, kwargs,
+                gas_limit, prior_tasks, reserves_task)
+
+        assert trans.task.uuid == uuid
+
+
     @pytest.mark.xfail(reason="SQL Testing Weirdness causes this to be problematic")
     def test_add_prior_tasks(self, db_session, persistence_int: SQLPersistenceInterface):
 
