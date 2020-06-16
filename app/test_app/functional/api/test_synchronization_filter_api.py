7871fe62099e71cae8df63b9b013ab4a32adc03e
michielderoos <michiel@deroos.ca>
2020-06-16 20:33:27 -0300

    Wee test fix

--- app/test_app/functional/api/test_synchronization_filter_api.py
+++ app/test_app/functional/api/test_synchronization_filter_api.py
@@ -7,16 +7,13 @@ from helpers.utils import will_func_test_blockchain
 @pytest.mark.parametrize("contract_address, contract_type, filter_type, filter_parameters, status_code", [
     (config.ETH_CONTRACT_ADDRESS, "ERC20", 'TRANSFER', None, 201),
 ])
-def test_sync_filter_api(test_client, create_ip_address, authed_sempo_admin_user, contract_address, contract_type, filter_type, filter_parameters, status_code):
+def test_sync_filter_api(test_client, create_ip_address, complete_admin_auth_token, contract_address, contract_type, filter_type, filter_parameters, status_code):
     # Super basic test for creation of sync filters
     # sync_filter_api is very basic right now, but there is added complexity on the roadmap
-    authed_sempo_admin_user.is_activated = True
-    auth_token = authed_sempo_admin_user.encode_auth_token().decode()
-
     response = test_client.post(
         '/api/v1/synchronization_filter/',
         headers=dict(
-            Authorization=auth_token,
+            Authorization=complete_admin_auth_token,
             Accept='application/json'
         ),
         json={
