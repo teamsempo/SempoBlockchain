d8e4117769c38d298babb603f299d48491d5eafb
michielderoos <michiel@deroos.ca>
2020-06-16 15:45:44 -0300

    Tweak the tests some more

--- app/test_app/functional/api/test_synchronization_filter_api.py
+++ app/test_app/functional/api/test_synchronization_filter_api.py
@@ -8,6 +8,8 @@ from helpers.utils import will_func_test_blockchain
     (config.ETH_CONTRACT_ADDRESS, "ERC20", 'TRANSFER', None, 201),
 ])
 def test_sync_filter_api(test_client, create_ip_address, authed_sempo_admin_user, contract_address, contract_type, filter_type, filter_parameters, status_code):
+    # Super basic test for creation of sync filters
+    # sync_filter_api is very basic right now, but there is added complexity on the roadmap
     authed_sempo_admin_user.is_activated = True
     auth_token = authed_sempo_admin_user.encode_auth_token().decode()
 
@@ -24,13 +26,8 @@ def test_sync_filter_api(test_client, create_ip_address, authed_sempo_admin_user
             'filter_parameters': filter_parameters
         })
 
+    assert response.json['contract_address'] == contract_address
+    assert response.json['contract_type'] == contract_type
+    assert response.json['filter_parameters'] == filter_parameters
+    assert response.json['filter_type'] == filter_type
 
-    if status_code == 201 and will_func_test_blockchain():
-        task_uuid = response.json['data']['exchange']['blockchain_task_uuid']
-        result = bt.await_task_success(task_uuid, timeout=config.SYNCRONOUS_TASK_TIMEOUT * 48)
-        assert response.status_code == status_code
-        assert result['status'] == 'SUCCESS'
-        assert response.json['contract_address'] == contract_address
-        assert response.json['contract_type'] == contract_type
-        assert response.json['filter_parameters'] == filter_parameters
-        assert response.json['filter_type'] == filter_type
