fa252cc331c4eb1be229d2583db707da1cf93093
michielderoos <michiel@deroos.ca>
2020-06-20 11:40:26 -0300

    Fix test

--- eth_worker/test_eth_worker/unit/conftest.py
+++ eth_worker/test_eth_worker/unit/conftest.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 import pytest
 from sqlalchemy.orm import scoped_session
 import redis
+from web3 import Web3
 
 import config
 from eth_manager.processor import TransactionProcessor
@@ -29,6 +30,12 @@ def persistence_module(db_session):
         red=None, session=db_session, first_block_hash='deadbeef01'
     )
 
+@pytest.fixture(scope='function')
+def processor(persistence_module, monkeypatch):
+    w3 = Web3()
+    
+    monkeypatch.setattr(w3.eth, "sendRawTransaction", lambda x: None)
+    monkeypatch.setattr(w3.eth, "sendRawTransaction", lambda x: None)
 
 @pytest.fixture(scope='function')
 def persistence_int(db_session):
