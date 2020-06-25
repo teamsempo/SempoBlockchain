83251a4874c44a4a32d57c90c15067cb6591b03a
Greg Taschuk <gregorytaschuk@gmail.com>
2020-06-24 22:42:32 -0700

    Convert transferAccount reducer and sagas to typescript (#298)
    * moved transferAccount reducer to follow common pattern
    
    * converted transfer account reducer and saga to typescript
    
    * added required fields to credit transfer fixture data
    
    * converted transfer account reducer and saga to typescript
    
    * bug fixes
    
    * improved typing
    
    * fix
    
    * bump
    
    Co-authored-by: Tristan Cole <tristanhcole@gmail.com>

---
+++ app/client/api/transferAccountAPI.ts
@@ -0,0 +1,27 @@
+import { apiClient } from "./client/apiClient";
+import {
+  EditTransferAccountPayload,
+  LoadTransferAccountListPayload
+} from "../reducers/transferAccount/types";
+
+export const loadTransferAccountListAPI = ({
+  query,
+  path
+}: LoadTransferAccountListPayload) =>
+  apiClient({
+    url: "/transfer_account/",
+    method: "GET",
+    query: query,
+    path: path
+  });
+
+export const editTransferAccountAPI = ({
+  body,
+  path
+}: EditTransferAccountPayload) =>
+  apiClient({
+    url: "/transfer_account/",
+    method: "PUT",
+    body: body,
+    path: path
+  });
--- app/client/reducers/transferAccount/types.ts
+++ app/client/reducers/transferAccount/types.ts
@@ -1,8 +1,32 @@
-//todo: complete when transfer account reducer converted to typescript
-export interface TransferAccount {}
+export interface TransferAccount {
+  id: string;
+}
 
 export interface TransferAccountByIDs {
   [key: number]: TransferAccount;
+}
+
+export enum TransferAccountActionTypes {
+  DEEP_UPDATE_TRANSFER_ACCOUNTS = "DEEP_UPDATE_TRANSFER_ACCOUNTS",
+  UPDATE_TRANSFER_ACCOUNTS_CREDIT_TRANSFERS = "UPDATE_TRANSFER_ACCOUNTS_CREDIT_TRANSFERS",
+  UPDATE_TRANSFER_ACCOUNTS = "UPDATE_TRANSFER_ACCOUNTS"
+}
+
+export enum LoadTransferAccountActionTypes {
+  LOAD_TRANSFER_ACCOUNTS_REQUEST = "LOAD_TRANSFER_ACCOUNTS_REQUEST",
+  LOAD_TRANSFER_ACCOUNTS_SUCCESS = "LOAD_TRANSFER_ACCOUNTS_SUCCESS",
+  LOAD_TRANSFER_ACCOUNTS_FAILURE = "LOAD_TRANSFER_ACCOUNTS_FAILURE"
+}
+
+export enum EditTransferAccountActionTypes {
+  EDIT_TRANSFER_ACCOUNT_REQUEST = "EDIT_TRANSFER_ACCOUNT_REQUEST",
+  EDIT_TRANSFER_ACCOUNT_SUCCESS = "EDIT_TRANSFER_ACCOUNT_SUCCESS",
+  EDIT_TRANSFER_ACCOUNT_FAILURE = "EDIT_TRANSFER_ACCOUNT_FAILURE"
+}
+
+export enum SetTransferAccountActionTypes {
+  SET_SELECTED = "SET_SELECTED",
+  RESET_SELECTED = "RESET_SELECTED"
 }
 
 export interface SingularTransferAccountData {
@@ -16,3 +40,40 @@ export interface MultipleTransferAccountData {
 export type TransferAccountData =
   | SingularTransferAccountData
   | MultipleTransferAccountData;
+
+export interface TransferAccountLoadApiResult {
+  type: typeof LoadTransferAccountActionTypes.LOAD_TRANSFER_ACCOUNTS_REQUEST;
+  payload: any;
+}
+
+export interface TransferAccountEditApiResult {
+  type: typeof EditTransferAccountActionTypes.EDIT_TRANSFER_ACCOUNT_REQUEST;
+  payload: any;
+}
+
+interface TransfersForUser {
+  credit_sends: string[]; // ids
+  credit_receives: string[];
+}
+
+export interface TransfersByUserId {
+  [userId: number]: TransfersForUser;
+}
+
+export interface LoadTransferAccountListPayload {
+  query?: {};
+  path?: number;
+}
+
+export interface EditTransferAccountPayload {
+  body: {
+    approve: boolean;
+    balance: number;
+    nfc_card_id?: string;
+    payable_period_length: number;
+    payable_period_type: string;
+    phone?: string;
+    qr_code?: string;
+  };
+  path: number;
+}
