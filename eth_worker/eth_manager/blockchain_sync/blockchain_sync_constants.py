587138a2d91f61133cecebcfebf61fb08e8b8e1f
michielderoos <michiel@deroos.ca>
2020-07-14 13:23:56 -0300

    Raise blocks per request

--- eth_worker/eth_manager/blockchain_sync/blockchain_sync_constants.py
+++ eth_worker/eth_manager/blockchain_sync/blockchain_sync_constants.py
@@ -1,4 +1,4 @@
-BLOCKS_PER_REQUEST = 100
+BLOCKS_PER_REQUEST = 1000
 # How long a blockchain_sync lock can last.
 # The lock gets renewed with every synchronized chunk, so the lock will continue to 
 # function in long-running jobs
