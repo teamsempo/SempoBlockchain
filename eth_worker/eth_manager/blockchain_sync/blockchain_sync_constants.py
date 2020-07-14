b0d505ac0fcea0a32cf60e97e30dc7796e3516a5
michielderoos <michiel@deroos.ca>
2020-07-14 10:08:22 -0300

    Alter constants

--- eth_worker/eth_manager/blockchain_sync/blockchain_sync_constants.py
+++ eth_worker/eth_manager/blockchain_sync/blockchain_sync_constants.py
@@ -1,5 +1,5 @@
-BLOCKS_PER_REQUEST = 500
+BLOCKS_PER_REQUEST = 100
 # How long a blockchain_sync lock can last.
 # The lock gets renewed with every synchronized chunk, so the lock will continue to 
 # function in long-running jobs
-LOCK_TIMEOUT = 120
+LOCK_TIMEOUT = 600
