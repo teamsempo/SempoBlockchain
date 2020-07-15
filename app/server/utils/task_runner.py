6367f3dfd0683bab5f375d6e43150e3668f6b6c5
michielderoos <michiel@deroos.ca>
2020-07-15 16:23:12 -0300

    some prints

--- app/server/utils/task_runner.py
+++ app/server/utils/task_runner.py
@@ -3,6 +3,10 @@ from flask import current_app
 from server import celery_app
 
 def delay_task(task, kwargs=None, args=None, force_simulate=False, queue='high-priority', task_uuid=None):
+    print("TASK RUNNER!")
+    print((task, kwargs, args))
+    print("TASK RUNNER!")
+
     if current_app.config['ENABLE_SIMULATOR_MODE'] or force_simulate:
         return worker_simulator.simulate(task, kwargs, args, queue)
     signature = celery_app.signature(task, kwargs=kwargs, args=args)
