c810dd5de3beb3567c597861cb9ce2877dc405e2
michielderoos <michiel@deroos.ca>
2020-07-16 14:02:30 -0300

    Nix old summing strat

--- app/server/models/daily_user_count.py
+++
@@ -1,8 +0,0 @@
-from server import db
-from server.models.utils import ModelBase
-import datetime
-
-class DailyUserCount(ModelBase):
-    __tablename__ = 'daily_user_count'
-    date = db.Column(db.DateTime)
-    count = db.Column(db.Integer)
