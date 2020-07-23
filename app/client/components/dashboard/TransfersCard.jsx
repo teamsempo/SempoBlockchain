88a753d63824d20a9680643ef4cad6e98ae4cb92
tristanhcole <tristanhcole@gmail.com>
2020-07-23 20:35:35 +1000

    adding todo

--- app/client/components/dashboard/TransfersCard.jsx
+++ app/client/components/dashboard/TransfersCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import VolumeChart from "./volumeChart";
 import GroupByChart from "./GroupByChart";
 
-import { Grid, Row, Col, Card, DatePicker, Tabs, Space, Statistic } from "antd";
+import { Card, DatePicker, Tabs, Statistic } from "antd";
 import {
   ArrowUpOutlined,
   ArrowDownOutlined,
@@ -16,6 +16,77 @@ import "./Tabs.css";
 
 export default class TransfersCard extends React.Component {
   render() {
+    const reduxState = {
+      participant_stats: {
+        filters: {
+          loadStatus: {
+            isRequesting: false,
+            success: false,
+            error: null
+          },
+          allowed_filters: {
+            "some object": "bar"
+          },
+          allowed_groupbys: {
+            "some object": "bar"
+          },
+          applied_filters: {
+            "some object": "foo"
+          },
+          applied_groupby: "gender",
+          selected_time_series: "volume"
+        },
+        data: {
+          loadStatus: {
+            isRequesting: false,
+            success: false,
+            error: null
+          },
+          aggregate: {
+            Tacos: 1040,
+            Milk: 142,
+            "SPY Puts": 1680
+          },
+          total: 2000,
+          timeseries: [
+            {
+              date: "2020-06-30T00:00:00",
+              values: {
+                Tacos: 100,
+                Milk: 20,
+                "SPY Puts": 900
+              }
+            },
+            {
+              date: "2020-07-01T00:00:00",
+              values: {
+                Tacos: 300,
+                Milk: 10,
+                "SPY Puts": 560
+              }
+            },
+            {
+              date: "2020-07-02T00:00:00",
+              values: {
+                Tacos: 320,
+                Milk: 56,
+                "SPY Puts": 110
+              }
+            },
+            {
+              date: "2020-07-03T00:00:00",
+              values: {
+                Tacos: 320,
+                Milk: 56,
+                "SPY Puts": 110
+              }
+            }
+          ]
+        }
+      },
+      transfer_stats: "similar"
+    };
+
     const data = {
       timeseries: [
         {
@@ -63,6 +134,8 @@ export default class TransfersCard extends React.Component {
         <RangePicker />
       </div>
     );
+
+    //todo: calculate %up/down based on first and last time series object data.
     const actions = [
       <Tabs
         defaultActiveKey="1"
