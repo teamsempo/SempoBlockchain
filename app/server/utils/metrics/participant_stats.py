3b9bd414fe62ecb8e7864385896180ec15db0049
michielderoos <michiel@deroos.ca>
2020-07-24 17:11:21 -0300

    add active users

--- app/server/utils/metrics/participant_stats.py
+++ app/server/utils/metrics/participant_stats.py
@@ -50,14 +50,14 @@ class ParticipantStats(metric_group.MetricGroup):
             filterable_by=self.filterable_attributes,
             timeseries_actions=[FORMAT_TIMESERIES, AGGREGATE_FORMATTED_TIMESERIES]))
 
-        # Special case query-- this is just used to calculate per_user
-        total_users_timeseries_query = db.session.query(func.count(User.id).label('volume'),
-                func.date_trunc(self.timeseries_unit, User.created).label('date')).group_by(func.date_trunc(self.timeseries_unit, User.created))
-        self.total_users_timeseries = metric.Metric(
-            metric_name='total_population',
-            query=total_users_timeseries_query,
-            object_model=User,
+        active_users_timeseries_query = db.session.query(func.count(func.distinct(CreditTransfer.sender_user_id)).label('volume'),
+                func.date_trunc(self.timeseries_unit, CreditTransfer.created).label('date'), group_strategy.group_by_column).group_by(func.date_trunc(self.timeseries_unit, CreditTransfer.created))
+        self.metrics.append(metric.Metric(
+            metric_name='active_users',  # Will rename when API breaking changes come in
+            query=group_strategy.build_query_group_by_with_join(active_users_timeseries_query, CreditTransfer),
+            object_model=CreditTransfer,
+            #stock_filters=[filters.beneficiary_filters],
             stock_filters=[],
-            caching_combinatory_strategy=metrics_cache.SUM_OBJECTS,
+            caching_combinatory_strategy=metrics_cache.QUERY_ALL,
             filterable_by=self.filterable_attributes,
-            timeseries_actions=[ADD_MISSING_DAYS_TO_TODAY, ACCUMULATE_TIMESERIES])
+            timeseries_actions=[FORMAT_TIMESERIES, AGGREGATE_FORMATTED_TIMESERIES]))
