a5616739afac2651b14daea2faa692eaeb167c87
Nick Williams <enjeyw@gmail.com>
2020-07-30 04:22:51 +1000

    More eth worker testing/Refactor (#297)
    * more refactoring
    
    * some work
    
    * adding more tests
    
    * fixing refactor of eth_worker celery running
    
    * changing eth worker build location
    
    * fix again
    
    * heaps of refactor!!
    
    * reverting docker compose
    
    * removing complete contracts
    
    * fixing imports
    
    * workaround annoying package not found issue in pycharm
    
    * working locally so should work in CI---- right?!!??!
    
    * rename src directory to prevent collisions in codecov
    
    * missed one place
    
    * another rename miss
    
    * grrr mock not behaving
    
    * basic functional tests
    
    * splitting up
    
    * renaming and pathing
    
    * refactoring of sigs
    
    * founding the missing bits :)
    
    * adding test for task duplication and clearing up some messaging
    
    * import fixes
    
    * putting back in task update
    
    * Update ganache_cli.sh
    
    * Update invoke_tests.py
    
    * refactors
    
    * getting tests to pass
    
    * bump
    
    * missing in git
    
    * add beat item

---
+++ eth_worker/eth_src/celery_app.py
@@ -0,0 +1,106 @@
+# https://stackoverflow.com/questions/54617308/pip-install-produces-the-following-error-on-mac-error-command-gcc-failed-wit
+# python3.7 / concurrent / futures/thread.py line 135 was originally self._work_queue = queue.SimpleQueue()
+from celery import Celery
+import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
+import redis, requests
+
+from requests.auth import HTTPBasicAuth
+from web3 import (
+    Web3,
+    HTTPProvider,
+    WebsocketProvider
+)
+
+from web3.exceptions import BadFunctionCallOutput
+
+import os
+import sys
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+grandparent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+sys.path.append(grandparent_dir)
+
+import config
+from sql_persistence import session
+from sql_persistence.interface import SQLPersistenceInterface
+
+from eth_manager.contract_registry.ABIs import (
+    dai_abi,
+    erc20_abi,
+    bancor_converter_abi,
+    bancor_network_abi
+)
+
+from eth_manager.eth_transaction_processor import EthTransactionProcessor
+from eth_manager.transaction_supervisor import TransactionSupervisor
+from eth_manager.task_manager import TaskManager
+from eth_manager.blockchain_sync.blockchain_sync import BlockchainSyncer
+
+import celery_utils
+
+sentry_sdk.init(config.SENTRY_SERVER_DSN, integrations=[CeleryIntegration()])
+
+ETH_CHECK_TRANSACTION_RETRIES = config.ETH_CHECK_TRANSACTION_RETRIES
+ETH_CHECK_TRANSACTION_RETRIES_TIME_LIMIT = config.ETH_CHECK_TRANSACTION_RETRIES_TIME_LIMIT
+ETH_CHECK_TRANSACTION_BASE_TIME = config.ETH_CHECK_TRANSACTION_BASE_TIME
+
+
+app = Celery('tasks',
+             broker=config.REDIS_URL,
+             backend=config.REDIS_URL,
+             task_serializer='json')
+
+app.conf.beat_schedule = {
+    "maintain_eth_balances": {
+        "task": celery_utils.eth_endpoint('topup_wallets'),
+        "schedule": 600.0
+    },
+}
+app.conf.beat_schedule = {
+    'third-party-transaction-sync': {
+        'task': celery_utils.eth_endpoint('synchronize_third_party_transactions'),
+        'schedule': 30, # Every 30 seconds
+    }
+}
+w3 = Web3(HTTPProvider(config.ETH_HTTP_PROVIDER))
+w3_websocket = Web3(WebsocketProvider(config.ETH_WEBSOCKET_PROVIDER))
+
+red = redis.Redis.from_url(config.REDIS_URL)
+
+first_block_hash = w3.eth.getBlock(0).hash.hex()
+
+persistence_module = SQLPersistenceInterface(
+    red=red, session=session, first_block_hash=first_block_hash
+)
+
+processor = EthTransactionProcessor(
+    ethereum_chain_id=config.ETH_CHAIN_ID,
+    gas_price_wei=w3.toWei(config.ETH_GAS_PRICE, 'gwei'),
+    gas_limit=config.ETH_GAS_LIMIT,
+    w3=w3,
+    persistence=persistence_module
+)
+
+supervisor = TransactionSupervisor(
+    red=red,
+    persistence=persistence_module,
+    processor=processor
+)
+
+task_manager = TaskManager(persistence=persistence_module, transaction_supervisor=supervisor)
+
+blockchain_sync = BlockchainSyncer(persistence=persistence_module, red=red, w3_websocket=w3_websocket)
+
+if os.environ.get('CONTAINER_TYPE') == 'PRIMARY':
+    persistence_module.create_blockchain_wallet_from_private_key(
+        config.MASTER_WALLET_PRIVATE_KEY,
+        allow_existing=True
+    )
+
+processor.registry.register_abi('ERC20', erc20_abi.abi)
+processor.registry.register_abi('bancor_converter', bancor_converter_abi.abi)
+processor.registry.register_abi('bancor_network', bancor_network_abi.abi)
+
+import celery_tasks
---
+++ eth_worker/eth_src/eth_manager/blockchain_sync/blockchain_sync.py
@@ -0,0 +1,170 @@
+import json
+from uuid import uuid4
+import config
+import requests
+from requests.auth import HTTPBasicAuth
+from math import ceil
+from web3 import Web3
+
+import eth_manager.blockchain_sync.blockchain_sync_constants as sync_const
+from eth_manager.contract_registry.ABIs import erc20_abi
+
+
+class BlockchainSyncer(object):
+
+    # Hit the database to get the latest block number to which we're synced
+    def get_latest_block_number(self):
+        return self.w3_websocket.eth.getBlock('latest').number
+
+    # Call app-level webhook with newfound transacitons
+    def call_webhook(self, transaction):
+        body = {
+            'sender_blockchain_address': transaction.sender_address,
+            'recipient_blockchain_address': transaction.recipient_address,
+            'blockchain_transaction_hash': transaction.hash,
+            'transfer_amount': int(transaction.amount),
+            'contract_address': transaction.contract_address
+        }
+        r = requests.post(config.APP_HOST + '/api/v1/credit_transfer/internal/',
+                            json=body,
+                            auth=HTTPBasicAuth(config.INTERNAL_AUTH_USERNAME,
+                                            config.INTERNAL_AUTH_PASSWORD),
+                            timeout=30
+                        )
+        return r
+
+    # Get list of filters from redis. This is the starting point of the synchronization process
+    def synchronize_third_party_transactions(self):
+        filters = self.persistence.get_all_synchronization_filters()
+        # Since the webook will timeout if we ask for too many blocks at once, we have to break
+        # the range we want into chunks. Once all the chunk-jobs are formed and loaded into redis,
+        # then trigger process_all_chunks, which will consume those jobs from redis
+        for f in filters:
+            # Make sure a filter is only being executed once at a time
+            have_lock = False
+            lock = self.red.lock(f'third-party-sync-lock-{f.id}', timeout=sync_const.LOCK_TIMEOUT)
+            try:
+                have_lock = lock.acquire(blocking=False)
+                if not have_lock:
+                    config.logg.info(f'Skipping execution of synchronizing filter {f.id}, as it is already running in another process')
+                    continue
+
+                latest_block = self.get_latest_block_number()
+                # If there's no filter.max_block (which is the default for auto-generated filters)
+                # start tracking third party transactions by looking at the lastest_block
+                max_fetched_block = f.max_block or latest_block
+                number_of_blocks_to_get = (latest_block - max_fetched_block)
+                number_of_chunks = ceil(number_of_blocks_to_get/sync_const.BLOCKS_PER_REQUEST)
+
+                for chunk in range(number_of_chunks):
+                    floor = max_fetched_block + (chunk * sync_const.BLOCKS_PER_REQUEST) + 1
+                    ceiling = max_fetched_block + ((chunk + 1) * sync_const.BLOCKS_PER_REQUEST)
+                    if ceiling > latest_block:
+                        ceiling = latest_block
+                    self.process_chunk(f, floor, ceiling)
+                    self.persistence.set_filter_max_block(f.id, ceiling)
+                    lock.reacquire()
+
+                if number_of_chunks == 0:
+                    self.persistence.set_filter_max_block(f.id, latest_block)
+            finally:
+                if have_lock:
+                    lock.reacquire()
+        return True
+
+    # Gets history for given range, and runs handle_event on all of them
+    # This is the second stage in the third party transaction processing pipeline!
+    def process_chunk(self, filter, floor, ceiling):
+        transaction_history = self.get_blockchain_transaction_history(
+                filter.contract_address,
+                floor,
+                ceiling,
+                filter.filter_parameters,
+                filter.id
+            )
+        for event in transaction_history:
+            self.handle_event(event, filter)
+
+    # Processes newly found transaction event
+    # Creates database object for transaction
+    # Calls webhook
+    # Sets sync status (whether or not webhook was successful)
+    # Fallback if something goes wrong at this level: `is_synchronized_with_app` flag. Can batch unsynced stuff
+    def handle_event(self, transaction, filter):
+        # Check if transaction already exists (I.e. already synchronized, or first party transactions)
+        transaction_object = self.persistence.get_transaction(hash=transaction.transactionHash.hex())
+        if transaction_object and transaction_object.is_synchronized_with_app:
+            return True
+        if not transaction_object:
+            transaction_object = self.persistence.create_external_transaction(
+                status = 'SUCCESS',
+                block = transaction.blockNumber,
+                hash = str(transaction.transactionHash.hex()),
+                contract_address = transaction.address,
+                is_synchronized_with_app = False,
+                recipient_address = transaction.args['to'],
+                sender_address = transaction.args['from'],
+                amount = float(transaction.args['value'])/(10**filter.decimals)*100 #To cents
+            )
+
+        webook_resp = self.call_webhook(transaction_object)
+        # Transactions which we fetched, but couldn't sync for whatever reason won't be marked as completed
+        # in order to be retryable later
+        if webook_resp.ok:
+            self.persistence.mark_transaction_as_completed(transaction_object)
+        return True
+
+    # Gets blockchain transaction history for given range
+    # Fallback if something goes wrong at this level: block-tracking table
+    def get_blockchain_transaction_history(self, contract_address, start_block, end_block = 'lastest', argument_filters = None, filter_id = None):
+        # Creates DB objects for every block to monitor status
+        config.logg.info(f'Fetching block range {start_block} to {end_block} for contract {contract_address}')
+
+        self.persistence.add_block_range(start_block, end_block, filter_id)
+        erc20_contract = self.w3_websocket.eth.contract(
+            address = Web3.toChecksumAddress(contract_address),
+            abi = erc20_abi.abi
+        )
+        try:
+            filter = erc20_contract.events.Transfer.createFilter(
+                fromBlock = start_block,
+                toBlock = end_block,
+                argument_filters = argument_filters
+            )
+            for event in filter.get_all_entries():
+                yield event
+            # Once a batch of chunks is completed, we can mark them completed
+            self.persistence.set_block_range_status(start_block, end_block, 'SUCCESS', filter_id)
+
+        except:
+            # Setting block status as a range since individual blocks can't fail at this stage (as we are getting a range of blocks)
+            self.persistence.set_block_range_status(start_block, end_block, 'FAILED', filter_id)
+            raise Exception(f'Failed fetching block range {start_block} to {end_block} for contract {contract_address}')
+
+    # Adds transaction filter to database if it doesn't already exist
+    def add_transaction_filter(self, contract_address, contract_type, filter_parameters, filter_type, decimals = 18, block_epoch=None):
+        # See if there's already a filter with the same contract address AND type. If there is, do nothing
+        # This lets you always add all filters at app-launch, without running an entire filter every time
+        if not contract_address:
+            raise Exception('No contract_address found for new contract filter')
+        # Block epoch being None is a sentinel for `latest`
+        if block_epoch == 'latest' or block_epoch is None:
+            epoch = None
+        else:
+            epoch = int(block_epoch)
+        if not self.persistence.check_if_synchronization_filter_exists(contract_address, filter_parameters):
+            # Set max_block to block_epoch to act as a de-factor zero-point
+            config.logg.info(f'No filter found for address {contract_address} with parameters {filter_parameters}. Creating.')
+            return self.persistence.add_transaction_filter(
+                contract_address,
+                contract_type,
+                filter_parameters,
+                filter_type, decimals,
+                block_epoch=epoch
+            )
+
+    def __init__(self, w3_websocket, red, persistence):
+
+        self.w3_websocket = w3_websocket
+        self.red = red
+        self.persistence = persistence
