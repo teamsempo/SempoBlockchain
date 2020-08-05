0dcbc9b40f5248e5432f7d8bd3b20fef0cf000bb
michielderoos <michiel@deroos.ca>
2020-08-05 14:06:40 -0300

    Update tf

--- terraform/sempo.tf
+++ terraform/sempo.tf
@@ -21,6 +21,15 @@ resource "aws_elb" "staging_lb" {
   connection_draining_timeout = 20
 }
 
+resource "aws_ecr_repository" "SempoRepository" {
+  name                 = "ecrrepo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 resource "aws_elastic_beanstalk_application" "SempoBlockchain" {
   name = "SempoBlockchain"
 }
@@ -32,6 +41,8 @@ resource "aws_elastic_beanstalk_environment" "staging_env" {
   solution_stack_name = "64bit Amazon Linux 2018.03 v2.20.4 running Multi-container Docker 19.03.6-ce (Generic)"
   wait_for_ready_timeout = "30m"
   cname_prefix           = "sempo-staging2"
+  depends_on = [aws_elastic_beanstalk_application.SempoBlockchain]
+
   setting {
       namespace = "aws:autoscaling:launchconfiguration"
       name = "IamInstanceProfile"
