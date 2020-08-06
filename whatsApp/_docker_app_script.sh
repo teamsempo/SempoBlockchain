e915a7db1f04e88728e1c4c4b41cbbdf50d2e23a
Tristan Cole <tristanhcole@gmail.com>
2020-08-06 15:21:39 +1000

    refactor: deprecating worker and WhatsApp (#402)

--- whatsApp/_docker_app_script.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-python whatsApp.py
--- worker/run_beat_if_bitcoin.py
+++
@@ -1,9 +0,0 @@
-import config
-import subprocess
-import os
-
-if config.IS_USING_BITCOIN:
-    DIR = os.path.abspath(os.path.dirname(__file__))
-    script = os.path.join(DIR,'_beat_starter.sh')
-    subprocess.call(['ls'])
-    subprocess.call([script])
