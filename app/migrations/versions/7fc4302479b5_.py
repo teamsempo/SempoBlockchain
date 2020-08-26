5ef856d0073ff020d5b06e874464de6507dcd105
Nick Williams <enjeyw@gmail.com>
2020-08-26 17:18:25 +1000

    merge heads

---
+++ app/migrations/versions/7fc4302479b5_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 7fc4302479b5
+Revises: 09edd72433d4, 2c3f97929457
+Create Date: 2020-08-26 17:17:57.080800
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7fc4302479b5'
+down_revision = ('09edd72433d4', '2c3f97929457')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
