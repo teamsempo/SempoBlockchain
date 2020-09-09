601fc2d57c226365545f8968a1ced3b5fdf832a2
michielderoos <michiel@deroos.ca>
2020-09-09 18:30:26 -0300

    two heads are worse than one

---
+++ app/migrations/versions/e99a825ac204_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e99a825ac204
+Revises: d3bff62150fa, 2c3f97929457
+Create Date: 2020-09-09 18:30:15.999919
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e99a825ac204'
+down_revision = ('d3bff62150fa', '2c3f97929457')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
