f0d33125e63d1c87e42fe25a95fb7889ce8a7ae0
michielderoos <michiel@deroos.ca>
2020-09-14 14:20:17 -0300

    pump up the test coverage

--- app/server/api/synchronization_filter_api.py
+++ app/server/api/synchronization_filter_api.py
@@ -13,22 +13,59 @@ class SynchronizationFilterAPI(MethodView):
     @requires_auth(allowed_roles={'ADMIN': 'admin'})
     def post(self):
         post_data = request.get_json()
-        call = post_data.get('call')
+        call = post_data.get('call', None)
+        if not call:
+            raise Exception('Please provide a \'call\' parameter')
 
-        #if call == ''
-        # add_transaction_filter
-        # refetch_block_range
-        # recall_webhook
-        contract_address = post_data.get('contract_address')
-        contract_type = post_data.get('contract_type')
-        filter_parameters = post_data.get('filter_parameters') 
-        filter_type = post_data.get('filter_type')      
-        decimals = post_data.get('decimals', 18)      
-        block_epoch = post_data.get('block_epoch', None)      
+        if call == 'add_transaction_filter':
+            """
+            Adds a new transaction filter for third party transaction sync
+            contract_address - Address for contract
+            contract_type - Type of contract to sync
+            filter_parameters - Filter params to pass to webhook
+            filter_type - Type of filter being used (E.g. TRANSFER, EXCHANGE)
+            decimals - decimals used for currency (default: 18)
+            block_epoch - Which block to use as zero-point in transaction sync. Use None to signifiy latest (Default: None)
+            """
+            contract_address = post_data.get('contract_address')
+            contract_type = post_data.get('contract_type')
+            filter_parameters = post_data.get('filter_parameters') 
+            filter_type = post_data.get('filter_type')      
+            decimals = post_data.get('decimals', 18)      
+            block_epoch = post_data.get('block_epoch', None)      
 
-        transaction_filter = add_transaction_filter(contract_address, contract_type, filter_parameters, filter_type, decimals = decimals, block_epoch = block_epoch)
+            transaction_filter = add_transaction_filter(contract_address, contract_type, filter_parameters, filter_type, decimals = decimals, block_epoch = block_epoch)
 
-        return make_response(jsonify(synchronization_filter_schema.dump(transaction_filter).data)), 201
+            return make_response(jsonify(synchronization_filter_schema.dump(transaction_filter).data)), 201
+
+        elif call == 'refetch_block_range':
+            """
+            Refetches a given range of blocks for third party transaction sync
+            Params:
+            filter_address - Address associated with a token.
+            floor - Where to start fetching blocks
+            ceiling - Where to finish fetching blocks
+            """
+            
+            filter_address = post_data.get('filter_address')
+            floor = int(post_data.get('floor'))
+            ceiling = int(post_data.get('ceiling'))
+            result = bt.force_fetch_block_range(filter_address, floor, ceiling)
+            return make_response(jsonify({ 'status': result })), 201
+
+        elif call == 'force_recall_webhook':
+            """
+            Forces a recall of the webhook for a given transaction
+            Params:
+            transaction_hash - Hash for the transaction you want to resync!
+            """
+            transaction_hash = post_data.get('transaction_hash')
+            result = bt.force_recall_webhook(transaction_hash)
+            return make_response(jsonify({ 'status': result })), 201
+
+        else:
+            raise Exception(f'{call} not a valid call')
+            
 
 class SynchronizationFilterMetricsAPI(MethodView):
     # Gets metrics regarding the status of third party sync
@@ -53,34 +90,6 @@ class SynchronizationFilterMetricsAPI(MethodView):
         resp['metrics'] = bt.get_third_party_sync_metrics()
         return make_response(jsonify(resp)), 201
 
-class SynchronizationFilterRefetchAPI(MethodView):
-    @requires_auth(allowed_roles={'ADMIN': 'admin'})
-    def post(self):
-        """
-        Refetches a given range of blocks for third party transaction sync
-        Params:
-        filter_address - Address associated with a token.
-        floor - Where to start fetching blocks
-        ceiling - Where to finish fetching blocks
-        """
-        filter_address = request.args.get('filter_address')
-        floor = int(request.args.get('floor'))
-        ceiling = int(request.args.get('ceiling'))
-        result = bt.force_fetch_block_range(filter_address, floor, ceiling)
-        return make_response(jsonify({ 'status': result })), 201
-
-class SynchronizationFilterRecallWebhookAPI(MethodView):
-    @requires_auth(allowed_roles={'ADMIN': 'admin'})
-    def post(self):
-        """
-        Forces a recall of the webhook for a given transaction
-        Params:
-        transaction_hash - Hash for the transaction you want to resync!
-        """
-        transaction_hash = request.args.get('transaction_hash')
-        bt.force_recall_webhook(transaction_hash)
-        
-
 synchronization_filter_blueprint.add_url_rule(
     '/synchronization_filter/',
     view_func=SynchronizationFilterAPI.as_view('synchronization_filter_view'),
@@ -93,14 +102,3 @@ synchronization_filter_blueprint.add_url_rule(
     methods=['GET']
 )
 
-synchronization_filter_blueprint.add_url_rule(
-    '/synchronization_filter/refetch_block_range/',
-    view_func=SynchronizationFilterRefetchAPI.as_view('synchronization_filter_refetch_view'),
-    methods=['POST']
-)
-
-synchronization_filter_blueprint.add_url_rule(
-    '/synchronization_filter/recall_webhook/',
-    view_func=SynchronizationFilterRecallWebhookAPI.as_view('synchronization_filter_recall_webhook_view'),
-    methods=['POST']
-)
