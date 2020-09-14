730c2265e31324a6380a68f1e3245a7990aec801
tristanhcole <tristanhcole@gmail.com>
2020-09-14 17:20:26 +1000

    small fix

--- app/client/components/intercom/IntercomSetup.tsx
+++ app/client/components/intercom/IntercomSetup.tsx
@@ -5,7 +5,7 @@ import { useIntercom } from "react-use-intercom";
 import { LoginState } from "../../reducers/auth/loginReducer";
 import { ReduxState } from "../../reducers/rootReducer";
 
-const IntercomSetup: React.FunctionComponent = props => {
+const IntercomSetup: React.FunctionComponent = () => {
   const { boot } = useIntercom();
   const loggedIn: boolean = useSelector(
     (state: ReduxState) => state.login.userId !== null
