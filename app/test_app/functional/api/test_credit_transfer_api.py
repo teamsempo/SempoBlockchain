3dfdfad247b5a0feba24398caa52f6f0c53447d6
michielderoos <michiel@deroos.ca>
2020-09-14 10:47:02 -0300

    fix test

--- app/test_app/functional/api/test_credit_transfer_api.py
+++ app/test_app/functional/api/test_credit_transfer_api.py
@@ -274,7 +274,7 @@ def test_credit_transfer_internal_callback(mocker, test_client, authed_sempo_adm
 
 def test_force_third_party_transaction_sync():
     if will_func_test_blockchain():
-        task_uuid = bt.force_third_party_transaction_()
+        task_uuid = bt.force_third_party_transaction_sync()
         bt.await_task_success(task_uuid, timeout=config.SYNCRONOUS_TASK_TIMEOUT * 48)
 
 @pytest.mark.parametrize("is_bulk, invert_recipient_list, transfer_amount, transfer_type, status_code", [
