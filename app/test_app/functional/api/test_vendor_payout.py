d0e6f8bdb9294dbb219c7de6e965763489f78471
michielderoos <michiel@deroos.ca>
2020-09-23 13:43:38 -0300

    Beef up the tests

--- app/test_app/functional/api/test_vendor_payout.py
+++ app/test_app/functional/api/test_vendor_payout.py
@@ -13,7 +13,7 @@ def test_get_vendor_payout(test_client, authed_sempo_admin_user, create_transfer
     user.set_held_role('VENDOR', 'supervendor')
     user.transfer_account.approve_and_disburse()
     user.transfer_account.organisation = authed_sempo_admin_user.organisations[0]
-    user.transfer_account.set_balance_offset(500000)
+    user.transfer_account.set_balance_offset(1000)
     db.session.commit()
 
     response = test_client.post(
@@ -30,6 +30,41 @@ def test_get_vendor_payout(test_client, authed_sempo_admin_user, create_transfer
     reader = csv.reader(f)
     formatted_results = list(reader)
     formatted_results[1][3] = 'SOME DATE'
+    print(formatted_results)
+    print('---')
+    print([
+        [
+            'ID', 
+            'First Name', 
+            'Last Name', 
+            'Created', 
+            'Current Balance', 
+            'Total Sent', 
+            'Total Received', 
+            'Approved', 
+            'Beneficiary', 
+            'Vendor', 
+            'Transaction ID', 
+            'Amount Due Today', 
+            'Payment Has Been Made', 
+            'Bank Payment Date'
+        ], [
+            '4', 
+            'Transfer', 
+            'User', 
+            'SOME DATE', 
+            '1000.0', 
+            '1000.000000000000', 
+            '0', 
+            'False', 
+            'False', 
+            'True', 
+            '1', 
+            '1000.0', 
+            '', 
+            ''
+        ]
+    ])
     assert formatted_results == [
         [
             'ID', 
@@ -51,14 +86,14 @@ def test_get_vendor_payout(test_client, authed_sempo_admin_user, create_transfer
             'Transfer', 
             'User', 
             'SOME DATE', 
-            '500000.0', 
-            '500000.000000000000', 
+            '1000.0', 
+            '1000.0000000000000000', 
             '0', 
             'False', 
             'False', 
             'True', 
             '1', 
-            '500000.0', 
+            '1000.0', 
             '', 
             ''
         ]
@@ -69,14 +104,65 @@ def test_get_vendor_payout(test_client, authed_sempo_admin_user, create_transfer
     assert transfer.recipient_transfer_account == user.transfer_account.token.float_account
     assert transfer.transfer_status == TransferStatusEnum.PENDING
 
-def test_process_vendor_payout_pend(test_client, authed_sempo_admin_user, create_transfer_account_user):
+def test_process_vendor_payout_approve(test_client, authed_sempo_admin_user, create_transfer_account_user):
     auth = get_complete_auth_token(authed_sempo_admin_user)
     user = create_transfer_account_user
     user.transfer_account.is_vendor = True
     user.set_held_role('VENDOR', 'supervendor')
     user.transfer_account.approve_and_disburse()
     user.transfer_account.organisation = authed_sempo_admin_user.organisations[0]
-    user.transfer_account.set_balance_offset(500000)
+    user.transfer_account.set_balance_offset(1000)
+    user.transfer_account.is_approved=True
+    db.session.commit()
+
+    test_client.post(
+        f"/api/v1/process_vendor_payout/",
+        headers=dict(
+            Authorization=auth,
+            Accept='application/json',
+        ),
+        json={
+            'csv_data':  """ID,First Name,Last Name,Created,Current Balance,Total Sent,Total Received,Approved,Beneficiary,Vendor,Transaction ID,Amount Due Today,Payment Has Been Made,Bank Payment Date\n4,Transfer,User,SOME DATE,1000.0,1000.000000000000,0,True,False,True,1,1000.0,TRUE,09/30/2020"""
+        }
+    )
+    transfer = user.transfer_account.credit_sends[0]
+    assert transfer.transfer_status == TransferStatusEnum.COMPLETE
+    transfer.transfer_status = TransferStatusEnum.PENDING
+
+def test_process_vendor_payout_reject(test_client, authed_sempo_admin_user, create_transfer_account_user):
+    auth = get_complete_auth_token(authed_sempo_admin_user)
+    user = create_transfer_account_user
+    user.transfer_account.is_vendor = True
+    user.set_held_role('VENDOR', 'supervendor')
+    user.transfer_account.approve_and_disburse()
+    user.transfer_account.organisation = authed_sempo_admin_user.organisations[0]
+    user.transfer_account.set_balance_offset(1000)
+    user.transfer_account.is_approved=True
+    db.session.commit()
+
+    test_client.post(
+        f"/api/v1/process_vendor_payout/",
+        headers=dict(
+            Authorization=auth,
+            Accept='application/json',
+        ),
+        json={
+            'csv_data':  """ID,First Name,Last Name,Created,Current Balance,Total Sent,Total Received,Approved,Beneficiary,Vendor,Transaction ID,Amount Due Today,Payment Has Been Made,Bank Payment Date\n4,Transfer,User,SOME DATE,1000.0,1000.000000000000,0,True,False,True,1,1000.0,FALSE,09/30/2020"""
+        }
+    )
+    transfer = user.transfer_account.credit_sends[0]
+    assert transfer.transfer_status == TransferStatusEnum.REJECTED
+    transfer.transfer_status = TransferStatusEnum.PENDING
+
+
+def test_process_vendor_payout_pending(test_client, authed_sempo_admin_user, create_transfer_account_user):
+    auth = get_complete_auth_token(authed_sempo_admin_user)
+    user = create_transfer_account_user
+    user.transfer_account.is_vendor = True
+    user.set_held_role('VENDOR', 'supervendor')
+    user.transfer_account.approve_and_disburse()
+    user.transfer_account.organisation = authed_sempo_admin_user.organisations[0]
+    user.transfer_account.set_balance_offset(1000)
     user.transfer_account.is_approved=True
     user.transfer_account.token.float_account.is_approved=True
     db.session.commit()
@@ -88,33 +174,10 @@ def test_process_vendor_payout_pend(test_client, authed_sempo_admin_user, create
             Accept='application/json',
         ),
         json={
-            'csv_data':  """ID,First Name,Last Name,Created,Current Balance,Total Sent,Total Received,Approved,Beneficiary,Vendor,Transaction ID,Amount Due Today,Payment Has Been Made,Bank Payment Date\n4,Transfer,User,SOME DATE,500000.0,500000.000000000000,0,True,False,True,1,500000.0,,09/30/2020"""
+            'csv_data':  """ID,First Name,Last Name,Created,Current Balance,Total Sent,Total Received,Approved,Beneficiary,Vendor,Transaction ID,Amount Due Today,Payment Has Been Made,Bank Payment Date\n4,Transfer,User,SOME DATE,1000.0,1000.000000000000,0,True,False,True,1,1000.0,,09/30/2020"""
         }
     )
     transfer = user.transfer_account.credit_sends[0]
 
     assert transfer.transfer_status == TransferStatusEnum.PENDING
-
-def test_process_vendor_payout_reject(test_client, authed_sempo_admin_user, create_transfer_account_user):
-    auth = get_complete_auth_token(authed_sempo_admin_user)
-    user = create_transfer_account_user
-    user.transfer_account.is_vendor = True
-    user.set_held_role('VENDOR', 'supervendor')
-    user.transfer_account.approve_and_disburse()
-    user.transfer_account.organisation = authed_sempo_admin_user.organisations[0]
-    user.transfer_account.set_balance_offset(500000)
-    user.transfer_account.is_approved=True
-    db.session.commit()
-
-    test_client.post(
-        f"/api/v1/process_vendor_payout/",
-        headers=dict(
-            Authorization=auth,
-            Accept='application/json',
-        ),
-        json={
-            'csv_data':  """ID,First Name,Last Name,Created,Current Balance,Total Sent,Total Received,Approved,Beneficiary,Vendor,Transaction ID,Amount Due Today,Payment Has Been Made,Bank Payment Date\n4,Transfer,User,SOME DATE,500000.0,500000.000000000000,0,True,False,True,1,500000.0,FALSE,09/30/2020"""
-        }
-    )
-    transfer = user.transfer_account.credit_sends[0]
-    assert transfer.transfer_status == TransferStatusEnum.REJECTED
+    transfer.transfer_status = TransferStatusEnum.PENDING
