dc13d968662c0306e212cd0453acb182687c3abe
Nick Williams <enjeyw@gmail.com>
2020-09-27 17:27:25 +1000

    Peer to peer fix (#453)
    * fixing peer-to-peer transfers
    
    * fixing peer-to-peer transfers
    
    * security fix and test
    
    * deploying to dev
    
    * version bump

--- app/test_app/functional/me_api/test_me_credit_transfer.py
+++ app/test_app/functional/me_api/test_me_credit_transfer.py
@@ -1,3 +1,5 @@
+import pytest, json, base64, config
+
 
 def test_get_me_credit_transfer_api(test_client, create_credit_transfer, create_transfer_account_user):
     """
@@ -13,3 +15,41 @@ def test_get_me_credit_transfer_api(test_client, create_credit_transfer, create_
                                content_type='application/json', follow_redirects=True)
     assert response.status_code == 201
     assert response.json['data']['credit_transfers'][0]['id'] is create_credit_transfer.id
+
+
+def test_invalid_create_me_credit_transfer_api(
+        test_client,
+        create_transfer_account_user,
+        create_transfer_account_user_2
+):
+    """
+    Tests that a user can only send a transfer from an account they own
+    """
+
+    auth_token = create_transfer_account_user.encode_auth_token()
+
+    ta1 = create_transfer_account_user.default_transfer_account
+    ta2 = create_transfer_account_user_2.default_transfer_account
+
+    ta1.is_approved = True
+    ta2.is_approved = True
+
+    ta1.set_balance_offset(1000)
+    ta2.set_balance_offset(1000)
+
+
+    response = test_client.post(
+        '/api/v1/me/credit_transfer/',
+        headers=dict(
+            Authorization=auth_token.decode(),
+            Accept='application/json'
+        ),
+        data=json.dumps(dict(
+            is_sending=True,
+            transfer_amount=10,
+            my_transfer_account_id=ta2.id,
+            transfer_account_id=ta1.id
+        )),
+        content_type='application/json', follow_redirects=True)
+
+    assert response.status_code == 401
