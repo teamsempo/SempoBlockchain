55348a489b3ae186fddac14e0284c96bfa2f1a06
michielderoos <michiel@deroos.ca>
2020-09-29 20:59:33 -0300

    merge noggins

---
+++ app/migrations/versions/57ca36524fed_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 57ca36524fed
+Revises: 23a2f59b78b9, 38f486afd1ae
+Create Date: 2020-09-29 20:59:19.937482
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '57ca36524fed'
+down_revision = ('23a2f59b78b9', '38f486afd1ae')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
