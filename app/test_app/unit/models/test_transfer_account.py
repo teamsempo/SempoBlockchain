e7f92194246daa972e33baa00cdab7bc07abd99c
Michiel <michiel@deroos.ca>
2020-10-08 07:16:40 -0300

    Fix initial disbursement logic (#487)
    * Fix balance transfers
    
    * Fix initial disbursements
    
    * Bump version
    
    * PR Fix
    
    * PR Fix
    
    * Make unit test much more better
    
    * bumperino
    
    Co-authored-by: Nick Williams <enjeyw@gmail.com>

--- app/test_app/unit/models/test_transfer_account.py
+++ app/test_app/unit/models/test_transfer_account.py
@@ -12,21 +12,6 @@ def test_create_transfer_account(create_transfer_account):
     assert create_transfer_account.blockchain_address is not None
 
 
-def test_approve_beneficiary_transfer_account(create_transfer_account_user, create_organisation):
-    """
-    GIVEN a Transfer Account model
-    WHEN a new transfer account is created AND approved
-    THEN check a BENEFICIARY is disbursed initial balance
-    """
-    create_transfer_account_user.transfer_account.is_beneficiary = True
-    g.active_organisation = create_organisation
-    create_transfer_account_user.transfer_account.approve_and_disburse(
-        initial_disbursement=create_organisation.default_disbursement
-    )
-
-    assert create_transfer_account_user.transfer_account.balance == create_organisation.default_disbursement
-
-
 def test_approve_vendor_transfer_account(create_transfer_account_user):
     """
     GIVEN a Transfer Account model
