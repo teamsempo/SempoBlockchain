bc6baf553cb4038e21cad48451bbce20e459620c
Michiel <michiel@deroos.ca>
2020-11-02 02:29:14 -0400

    Sharding for transfer cards (#499)
    * serverside shard!
    
    * version
    
    * Frontend and API
    
    * Copy change
    
    * Mark units in comments
    
    * PR changes
    
    * Add test to check place names
    
    * index location too
    
    * debug
    
    * Ensure user has lat and lon
    
    * remove prints
    
    * .
    
    * fix
    
    * assert just to clarify test cond
    
    * merging heads
    
    * fixing merge
    
    * bump
    
    Co-authored-by: Nick Williams <enjeyw@gmail.com>

---
+++ app/migrations/versions/a2c6e9c85a69_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a2c6e9c85a69
+Revises: fbe44b3db548, 3de8e29dd5c4
+Create Date: 2020-11-02 15:44:12.526048
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a2c6e9c85a69'
+down_revision = ('fbe44b3db548', '3de8e29dd5c4')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
