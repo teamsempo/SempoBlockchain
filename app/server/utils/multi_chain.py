df1d830f7b9d18cab057bd11f50dce327548b445
michielderoos <michiel@deroos.ca>
2020-11-17 11:26:11 -0400

    More config

--- app/server/utils/multi_chain.py
+++ app/server/utils/multi_chain.py
@@ -1,5 +1,4 @@
-from flask import g
-import config
+from flask import g, current_app
 
 def get_chain():
-    return g.active_organisation.token.chain if g.get('active_organisation', False) and g.active_organisation.token else config.DEFAULT_CHAIN
+    return g.active_organisation.token.chain if g.get('active_organisation', False) and g.active_organisation.token else current_app.config['DEFAULT_CHAIN']
