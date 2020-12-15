6c440d5f6c0be2d18e076f02f24c60542a5eea22
tristanhcole <tristanhcole@gmail.com>
2020-12-15 17:52:52 +1100

    improve styling

--- app/client/components/pages/mapPage.jsx
+++ app/client/components/pages/mapPage.jsx
@@ -1,5 +1,6 @@
 import React, { lazy } from "react";
 import { connect } from "react-redux";
+import { Card } from "antd";
 import { WrapperDiv, PageWrapper } from "../styledElements";
 import LoadingSpinner from "../loadingSpinner.jsx";
 import FilterModule from "../filterModule/FilterModule";
@@ -21,12 +22,22 @@ class MapPage extends React.Component {
     return (
       <WrapperDiv>
         <PageWrapper>
-          <FilterModule
-            defaultGroupBy={"sender,coordinates"}
-            filterObject={"credit_transfer"}
-            dateRange={null}
-            hideGroupBy={true}
-          />
+          <Card
+            style={{
+              position: "fixed",
+              top: 0,
+              zIndex: 1,
+              margin: "1em",
+              maxWidth: "calc(100vw - 2em)"
+            }}
+          >
+            <FilterModule
+              defaultGroupBy={"sender,coordinates"}
+              filterObject={"credit_transfer"}
+              dateRange={null}
+              hideGroupBy={true}
+            />
+          </Card>
           {this.props.metricsLoadStatus.success ? (
             <HeatMap />
           ) : (
