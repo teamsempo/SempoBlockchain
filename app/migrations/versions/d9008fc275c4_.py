ff54b0c89c62b45469fc28c3751512adc4e7919e
tristanhcole <tristanhcole@gmail.com>
2021-01-14 13:59:05 +1100

    merge heads

---
+++ app/migrations/versions/d9008fc275c4_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: d9008fc275c4
+Revises: 33df5e72fca4, 0a3d6bae9a9b
+Create Date: 2021-01-14 13:58:35.527966
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'd9008fc275c4'
+down_revision = ('33df5e72fca4', '0a3d6bae9a9b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
