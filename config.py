8e79cfde45cf38a96409900e78a86948988fdace
tristanhcole <tristanhcole@gmail.com>
2021-01-14 12:51:53 +1100

    bump

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ env_loglevel = os.environ.get('LOGLEVEL', 'DEBUG')
 logging.basicConfig(level=env_loglevel)
 logg = logging.getLogger(__name__)
 
-VERSION = '1.7.52'  # Remember to bump this in every PR
+VERSION = '1.7.53'  # Remember to bump this in every PR
 
 logg.info('Loading configs at UTC {}'.format(datetime.datetime.utcnow()))
 
