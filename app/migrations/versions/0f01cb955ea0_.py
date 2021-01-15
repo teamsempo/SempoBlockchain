911c92616143667d6e60a3e96f03e4829702f9e6
Michiel <michiel@deroos.ca>
2021-01-15 09:46:18 -0400

    Tidy database migrations (#554)
    * Clean up migrations
    
    * version
    
    * do nothing
    
    * Ensure
    
    * index_exists hack
    
    * merge heads

---
+++ app/migrations/versions/0f01cb955ea0_.py
@@ -0,0 +1,24 @@
+"""merge some heads
+
+Revision ID: 0f01cb955ea0
+Revises: d9008fc275c4, c8395e165794
+Create Date: 2021-01-15 09:09:33.915151
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0f01cb955ea0'
+down_revision = ('d9008fc275c4', 'c8395e165794')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
