6c61f0e4a25ea18202dd69e6935ba045b596c654
michielderoos <michiel@deroos.ca>
2021-01-20 13:40:57 -0400

    Fix test

--- app/server/__init__.py
+++ app/server/__init__.py
@@ -145,17 +145,26 @@ def register_blueprints(app):
             after_request_transactions.append((transaction.__class__, transaction.id, queue))
         # response.on_call_close will execute _after_ the call returns. This makes it so the user doesn't 
         # have to wait for send_blockchain_payload_to_worker to finish for each transaction 
-        #@response.call_on_close
-        #def process_after_request():
-        #    with app.app_context():
-        for transaction_class, transaction_id, queue in after_request_transactions:
-            transaction = db.session.query(transaction_class)\
-                .filter(transaction_class.id == transaction_id)\
-                .execution_options(show_all=True)\
-                .first()
-            transaction.send_blockchain_payload_to_worker(queue=queue)
-            # DB is modified, so commit changes
-            db.session.commit()
+        def process_after_request_transactions(after_request_transactions):
+            for transaction_class, transaction_id, queue in after_request_transactions:
+                transaction = db.session.query(transaction_class)\
+                    .filter(transaction_class.id == transaction_id)\
+                    .execution_options(show_all=True)\
+                    .first()
+                transaction.send_blockchain_payload_to_worker(queue=queue)
+                # DB is modified, so commit changes
+                db.session.commit()
+
+        # on_call_close doesn't run in the test environment so process transactions here in that case
+        if config.IS_TEST and after_request_transactions:
+            process_after_request_transactions(after_request_transactions)
+
+        @response.call_on_close
+        def process_after_request():
+            if after_request_transactions:
+                with app.app_context():
+                    if not config.IS_TEST:
+                        process_after_request_transactions(after_request_transactions)
 
         # Push only credit transfers, not exchanges
         from server.models.credit_transfer import CreditTransfer
