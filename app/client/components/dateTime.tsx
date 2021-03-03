61c1e3be22efe50d983819379ae16a4d70a8e0c7
michielderoos <michiel@deroos.ca>
2021-03-03 13:55:25 -0400

    Change to real time

--- app/client/components/dateTime.tsx
+++ app/client/components/dateTime.tsx
@@ -16,10 +16,13 @@ interface Props {
 export default function DateTime(props: Props) {
   const { created } = props;
   if (created) {
-    var formatted_time = moment.utc(created).fromNow();
+    var formatted_time = moment
+      .utc(created)
+      .local()
+      .format("YYYY-MM-DD HH:mm:ss");
 
     return <div style={{ margin: 0 }}>{formatted_time}</div>;
   } else {
-    return <div style={{ margin: 0 }}>Not long ago</div>;
+    return <div style={{ margin: 0 }}>Unknown</div>;
   }
 }
