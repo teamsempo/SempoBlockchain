8ee6774b1787dbf701ccb8845918242b880240c8
michielderoos <michiel@deroos.ca>
2021-03-24 16:03:36 -0300

    unused import

--- app/client/components/transferAccount/StandardTransferAccountList.tsx
+++ app/client/components/transferAccount/StandardTransferAccountList.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { connect, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import { Modal, Button, InputNumber, Space, Select } from "antd";
 const { Option } = Select;
 
