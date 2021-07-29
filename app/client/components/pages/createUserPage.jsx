27003dc80ad2248ca07984733ae97aec9ad89295
Tristan Cole <tristanhcole@gmail.com>
2021-07-30 09:42:58 +1000

    refactor: create user page (#735)
    * bug fix
    
    * refactor: create user page
    
    * small changes
    
    * bump
    
    * fix phone

--- app/client/components/pages/createUserPage.jsx
+++ app/client/components/pages/createUserPage.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 
-import { WrapperDiv } from "../styledElements";
-
-import { PageWrapper, ModuleBox } from "../styledElements.js";
 import CreateUserUpdated from "../user/CreateUser.tsx";
 
 export default class createUserPage extends React.Component {
   render() {
     return (
-      <WrapperDiv>
-        <PageWrapper>
-          <ModuleBox>
-            <CreateUserUpdated />
-          </ModuleBox>
-        </PageWrapper>
-      </WrapperDiv>
+      <div style={{ padding: "16px 0" }}>
+        <CreateUserUpdated />
+      </div>
     );
   }
 }
