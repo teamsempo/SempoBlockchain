a4031dd11148b743ba367a1f4ba88819d16cd9b1
michielderoos <michiel@deroos.ca>
2021-08-25 19:17:27 -0300

    Simplify build script, hopefully this runs

--- app/_docker_app_script.sh
+++ app/_docker_app_script.sh
@@ -1,37 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 echo Container mode: $CONTAINER_MODE
 
 cd app
 echo upgrading database
 python manage.py db upgrade
 
-ret=$?
-if [ "$ret" -ne 0 ]; then
-  exit $ret
-fi
+echo upgrading dataset
 
-ret=$?
-if [ "$ret" -ne 0 ]; then
-  exit $ret
-fi
-
-if [ "$CONTAINER_MODE" == 'TEST' ]; then
-   coverage run -m pytest test_app -v
-   ret=$?
-   if [ "$ret" -ne 0 ]; then
-     exit $ret
-   fi
-   if [ ! -z "$CODECOV_TOKEN" ]; then # only report if CODECOV_TOKEN is set
-     bash <(curl -s https://codecov.io/bash) -cF python
-   fi
-elif [ "$CONTAINER_MODE" == 'ETH_WORKER_TEST' ]; then
-   echo pass
-   sleep infinity
-else
-
-  echo upgrading dataset
-
-  python manage.py update_data
-
-fi
+python manage.py update_data
 
