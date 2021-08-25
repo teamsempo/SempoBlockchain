69903477eb4135ab41e8c527174f893728cf3f58
michielderoos <michiel@deroos.ca>
2021-08-25 11:53:20 -0300

    test

--- app/client/components/navBar/index.tsx
+++ app/client/components/navBar/index.tsx
@@ -119,7 +119,7 @@ class NavBar extends React.Component<Props, State> {
             <SubMenu
               key="sub1"
               icon={<DesktopOutlined translate={""} />}
-              title="Dashboard"
+              title="DASHO"
             >
               <Menu.Item key="/">
                 <NavLink to="/">
