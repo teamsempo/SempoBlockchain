f0c15f0305ed316fa48b7b3447e91ffbd7366112
michielderoos <michiel@deroos.ca>
2021-12-08 16:21:27 -0400

    version

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ env_loglevel = os.environ.get('LOGLEVEL', 'DEBUG')
 logging.basicConfig(level=env_loglevel)
 logg = logging.getLogger(__name__)
 
-VERSION = '2.0.6'  # Remember to bump this in every PR
+VERSION = '2.0.7'  # Remember to bump this in every PR
 
 logg.info('Loading configs at UTC {}'.format(datetime.datetime.utcnow()))
 
