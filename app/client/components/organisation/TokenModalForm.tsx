da60382b9da6e763e549d40fa2372ed1410b8834
michielderoos <michiel@deroos.ca>
2021-12-09 16:18:00 -0400

    interesting

--- app/client/components/organisation/TokenModalForm.tsx
+++ app/client/components/organisation/TokenModalForm.tsx
@@ -127,7 +127,6 @@ const TokenModalForm: React.FC<IProps> = props => {
 };
 
 const mapStateToProps = (state: any): StateProps => {
-  console.log("abc");
   return {
     tokens: state.tokens
   };
