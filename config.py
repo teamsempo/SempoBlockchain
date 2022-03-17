ebde8ba7dd16dc6760277644e3b2ec62d2aae269
Michiel <michiel@deroos.ca>
2022-03-17 14:42:59 -0300

    Invalidate auth tokens based on IAT (#790)

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=env_loglevel)
 logg = logging.getLogger(__name__)
 
 
-VERSION = '2.1.4'  # Remember to bump this in every PR
+VERSION = '2.1.5'  # Remember to bump this in every PR
 
 logg.info('Loading configs at UTC {}'.format(datetime.datetime.utcnow()))
 
@@ -152,7 +152,7 @@ THIRD_PARTY_SYNC_EPOCH = config_parser['APP'].get('THIRD_PARTY_SYNC_EPOCH', 'lat
 THIRD_PARTY_SYNC_CHECK_PERIOD_SECONDS = int(config_parser['APP'].get('THIRD_PARTY_SYNC_CHECK_PERIOD_SECONDS', '60'))
 
 SINGLE_USE_TOKEN_EXPIRATION      = 60 * 60 * 24 * 1
-AUTH_TOKEN_EXPIRATION = int(config_parser['APP'].getboolean('AUTH_TOKEN_EXPIRATION', 60 * 60 * 24 * 7))  # 1 week
+AUTH_TOKEN_EXPIRATION = int(config_parser['APP'].getboolean('AUTH_TOKEN_EXPIRATION', 60 * 60 * 2))  # 2 Hours
 VERIFY_JWT_EXPIRY     = config_parser['APP'].getboolean('VERIFY_JWT_EXPIRY', True)
 PASSWORD_PEPPER       = secrets_parser['APP'].get('PASSWORD_PEPPER')
 SECRET_KEY            = secrets_parser['APP']['SECRET_KEY'] + DEPLOYMENT_NAME
