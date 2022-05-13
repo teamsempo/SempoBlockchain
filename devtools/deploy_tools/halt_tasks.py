9f87fee4d95c5073bb93663e5f3722080e7c2bd4
michielderoos <michiel@deroos.ca>
2022-05-13 16:02:02 -0300

    Updated script

--- devtools/deploy_tools/halt_tasks.py
+++ devtools/deploy_tools/halt_tasks.py
@@ -4,7 +4,7 @@ import sys
 
 cluster = sys.argv[1]
 region = sys.argv[2]
-stream = os.popen('aws ecs list-tasks --cluster '+ cluster)
+stream = os.popen(f'aws ecs list-tasks --cluster {cluster} --region {region}')
 task_list = json.loads(stream.read())['taskArns']
 for task in task_list:
     os.popen(f'aws ecs stop-task --task {task} --cluster {cluster} --region {region}')
