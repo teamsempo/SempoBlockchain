0d24fbbfb609bfbff933a4a458fce68643cd8f27
michielderoos <michiel@deroos.ca>
2022-05-13 10:31:42 -0300

    Update config

--- devtools/deploy_tools/halt_tasks.py
+++ devtools/deploy_tools/halt_tasks.py
@@ -8,3 +8,5 @@ stream = os.popen('aws ecs list-tasks --cluster '+ cluster)
 task_list = json.loads(stream.read())['taskArns']
 for task in task_list:
     os.popen(f'aws ecs stop-task --task {task} --cluster {cluster}')
+    output = stream.read()
+    print(output)
