bb97f54e410ed908e8ad6bdb3a36213dd943ce09
michielderoos <michiel@deroos.ca>
2022-07-25 17:03:28 -0300

    Bump versions

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=env_loglevel)
 logg = logging.getLogger(__name__)
 
 
-VERSION = '2.1.17'  # Remember to bump this in every PR
+VERSION = '2.1.18'  # Remember to bump this in every PR
 
 logg.info('Loading configs at UTC {}'.format(datetime.datetime.utcnow()))
 
